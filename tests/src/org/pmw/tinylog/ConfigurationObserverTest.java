8ddb6856efe8fb6d2a451e4e7497a0836d35dba7
Martin Winandy <martin.winandy@gmx.de>
2014-07-12 14:10:50 +0200

    Avoid timing problems while waiting for configuration observer

--- tests/src/org/pmw/tinylog/ConfigurationObserverTest.java
+++ tests/src/org/pmw/tinylog/ConfigurationObserverTest.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2013 Martin Winandy
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -622,6 +622,7 @@ public class ConfigurationObserverTest extends AbstractTest {
 			while (observer.getState() == Thread.State.RUNNABLE) {
 				Thread.sleep(10);
 			}
+			Thread.sleep(10);
 		}
 	}
 
