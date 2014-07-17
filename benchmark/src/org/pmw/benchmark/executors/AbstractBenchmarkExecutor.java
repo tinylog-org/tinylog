563d617023f15cb5c96b277131c723a328c17401
Martin Winandy <martin.winandy@gmx.de>
2014-07-17 15:21:43 +0200

    Output average time of benchmarks

--- benchmark/src/org/pmw/benchmark/executors/AbstractBenchmarkExecutor.java
+++ benchmark/src/org/pmw/benchmark/executors/AbstractBenchmarkExecutor.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2014 Martin Winandy
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -60,8 +60,8 @@ public abstract class AbstractBenchmarkExecutor {
 			times[i] = finished - start;
 		}
 
-		long time = calculateTotalTime(times);
-		long countedIterations = (runs - outliers) * countTriggeredLogEntries();
+		long time = Math.round((double) calculateTotalTime(times) / (runs - outliers));
+		long countedIterations = countTriggeredLogEntries();
 		long iterationsPerSecond = Math.round(countedIterations * 1000d / time);
 
 		System.out.println(MessageFormat.format(RESULT_MESSAGE, benchmark.getName(), getName(), countedIterations, time, iterationsPerSecond));
