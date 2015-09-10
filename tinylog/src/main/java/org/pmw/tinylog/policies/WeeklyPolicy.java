e6054be2ea93cdc1301ccf8ab4fc63cd1ab9cd39
Robert Elek <robert@apn2.com>
2015-09-10 10:41:54 +0700

    handle WeeklyPolicy numerical parameters consistently, eg. 1=Monday always

--- tinylog/src/main/java/org/pmw/tinylog/policies/WeeklyPolicy.java
+++ tinylog/src/main/java/org/pmw/tinylog/policies/WeeklyPolicy.java
@@ -71,7 +71,7 @@ public final class WeeklyPolicy extends AbstractTimeBasedPolicy {
 	}
 
 	private static int convert(final Calendar calendar, final int dayOfWeek) {
-		return (calendar.getFirstDayOfWeek() - 1 + dayOfWeek - 1) % 7 + 1;
+		return (Calendar.MONDAY - 1 + dayOfWeek - 1) % 7 + 1;
 	}
 
 	private static Calendar createCalendar(final String dayOfWeek) {
