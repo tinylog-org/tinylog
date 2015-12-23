54d023c305916fa9d29499c4d59dfb66a3a4c1b9
Martin Winandy <martin.winandy@pmwmedia.de>
2015-12-23 11:34:47 +0100

    Simplify tinylog bridge

--- jcl-binding/src/main/java/org/tinylog/commons/logging/TinylogBridge.java
+++ jcl-binding/src/main/java/org/tinylog/commons/logging/TinylogBridge.java
@@ -25,29 +25,7 @@ import org.tinylog.Logger;
  */
 final class TinylogBridge {
 
-	private static Method stackTraceMethod;
-	private static boolean hasSunReflection;
-
-	static {
-		try {
-			stackTraceMethod = Throwable.class.getDeclaredMethod("getStackTraceElement", int.class);
-			stackTraceMethod.setAccessible(true);
-			StackTraceElement stackTraceElement = (StackTraceElement) stackTraceMethod.invoke(new Throwable(), 0);
-			if (!TinylogBridge.class.getName().equals(stackTraceElement.getClassName())) {
-				stackTraceMethod = null;
-			}
-		} catch (Exception ex) {
-			stackTraceMethod = null;
-		}
-
-		try {
-			@SuppressWarnings({ "restriction", "deprecation" })
-			Class<?> caller = sun.reflect.Reflection.getCallerClass(1);
-			hasSunReflection = TinylogBridge.class.equals(caller);
-		} catch (Exception ex) {
-			hasSunReflection = false;
-		}
-	}
+	private static final int DEEP_OF_STACK_TRACE = 2;
 
 	private TinylogBridge() {
 	}
@@ -60,9 +38,7 @@ final class TinylogBridge {
 	 * @return <code>true</code> if log entries with the given logging level will be output, <code>false</code> if not
 	 */
 	public static boolean isEnabled(final Level level) {
-		String className = getFullyQualifiedClassName(3);
-		Level activeLevel = Logger.getLevel(className);
-		return activeLevel.ordinal() <= level.ordinal();
+		return LogEntryForwarder.isEnabled(DEEP_OF_STACK_TRACE, level);
 	}
 
 	/**
@@ -74,7 +50,7 @@ final class TinylogBridge {
 	 *            Message to log
 	 */
 	public static void log(final Level level, final Object message) {
-		LogEntryForwarder.forward(2, level, message);
+		LogEntryForwarder.forward(DEEP_OF_STACK_TRACE, level, message);
 	}
 
 	/**
@@ -88,29 +64,7 @@ final class TinylogBridge {
 	 *            Throwable to log
 	 */
 	public static void log(final Level level, final Object message, final Throwable throwable) {
-		LogEntryForwarder.forward(2, level, throwable, message == null ? null : message.toString());
-	}
-
-	@SuppressWarnings({ "restriction", "deprecation" })
-	private static String getFullyQualifiedClassName(final int deep) {
-		if (hasSunReflection) {
-			try {
-				return sun.reflect.Reflection.getCallerClass(deep + 1).getName();
-			} catch (Exception ex) {
-				InternalLogger.warn(ex, "Failed to get caller class from ");
-			}
-		}
-
-		if (stackTraceMethod != null) {
-			try {
-				return ((StackTraceElement) stackTraceMethod.invoke(new Throwable(), deep)).getClassName();
-			} catch (Exception ex) {
-				InternalLogger.warn(ex, "Failed to get single stack trace element from throwable");
-			}
-		}
-
-		StackTraceElement[] stackTraceElements = new Throwable().getStackTrace();
-		return stackTraceElements[deep].getClassName();
+		LogEntryForwarder.forward(DEEP_OF_STACK_TRACE, level, message == null ? null : message.toString(), throwable);
 	}
 
 }
