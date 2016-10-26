7238b49e92139ae8abee44c68a1d3a36704c8a24
Martin Winandy <martin.winandy@pmwmedia.de>
2016-10-26 19:33:01 +0200

    Add NOP logging provider implementation that does nothing

---
+++ tinylog-api/src/main/java/org/tinylog/provider/NopLoggingProvider.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.provider;
+
+import org.tinylog.Level;
+
+/**
+ * Logging provider implementation that does nothing. All log entries will be ignored.
+ */
+final class NopLoggingProvider implements LoggingProvider {
+
+	/** */
+	NopLoggingProvider() {
+	}
+
+	@Override
+	public Level getMinimumLevel() {
+		return Level.OFF;
+	}
+
+	@Override
+	public boolean isEnabled(final int depth, final Level level) {
+		return false;
+	}
+
+	@Override
+	public void log(final int depth, final Level level, final Throwable exception, final Object obj, final Object... arguments) {
+		// Ignore
+	}
+
+	@Override
+	public void internal(final int depth, final Level level, final Throwable exception, final Object obj, final Object... arguments) {
+		// Ignore
+	}
+
+}
---
+++ tinylog-api/src/test/java/org/tinylog/provider/NopLoggingProviderTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.provider;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.tinylog.Level;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link NopLoggingProvider}.
+ */
+public final class NopLoggingProviderTest {
+
+	private NopLoggingProvider provider;
+
+	/**
+	 * Initializes NOP logging provider.
+	 */
+	@Before
+	public void init() {
+		provider = new NopLoggingProvider();
+	}
+
+	/**
+	 * Verifies that the minimum severity level is {@link Level#OFF}.
+	 */
+	@Test
+	public void getMinimumLevel() {
+		assertThat(provider.getMinimumLevel()).isEqualTo(Level.OFF);
+	}
+
+	/**
+	 * Verifies that all severity levels are disabled.
+	 */
+	@Test
+	public void isEnabled() {
+		assertThat(provider.isEnabled(0, Level.TRACE)).isFalse();
+		assertThat(provider.isEnabled(1, Level.DEBUG)).isFalse();
+		assertThat(provider.isEnabled(2, Level.INFO)).isFalse();
+		assertThat(provider.isEnabled(3, Level.WARN)).isFalse();
+		assertThat(provider.isEnabled(4, Level.ERROR)).isFalse();
+	}
+
+	/**
+	 * Verifies that log() method is invokable without throwing any exceptions.
+	 */
+	@Test
+	public void log() {
+		provider.log(0, Level.DEBUG, null, null, (Object[]) null);
+		provider.log(1, Level.ERROR, null, null, (Object[]) null);
+	}
+
+	/**
+	 * Verifies that internal() method is invokable without throwing any exceptions.
+	 */
+	@Test
+	public void internal() {
+		provider.internal(0, Level.DEBUG, null, null, (Object[]) null);
+		provider.internal(1, Level.ERROR, null, null, (Object[]) null);
+	}
+
+}
