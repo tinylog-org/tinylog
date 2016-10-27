7bb00b8df1c6f9563d1b3dd690e0790365e889a0
Martin Winandy <martin.winandy@pmwmedia.de>
2016-10-27 21:52:40 +0200

    Add internal logger for logging internal warnings and errors

---
+++ tinylog-api/src/main/java/org/tinylog/provider/InternalLogger.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2016 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.provider;
+
+import org.tinylog.Level;
+
+/**
+ * Static logger for logging internal warnings and errors to console.
+ */
+public final class InternalLogger {
+
+	private static final int BUFFER_SIZE = 256;
+
+	/** */
+	private InternalLogger() {
+	}
+
+	/**
+	 * Logs a plain text message.
+	 *
+	 * @param level
+	 *            Severity level
+	 * @param message
+	 *            Plain text message
+	 */
+	public static void log(final Level level, final String message) {
+		System.err.println("LOGGER " + level + ": " + message);
+	}
+
+	/**
+	 * Logs a caught exception.
+	 *
+	 * @param level
+	 *            Severity level
+	 * @param exception
+	 *            Caught exception
+	 */
+	public static void log(final Level level, final Throwable exception) {
+		String nameOfException = exception.getClass().getName();
+		String messageOfException = exception.getMessage();
+
+		if (messageOfException == null || messageOfException.isEmpty()) {
+			System.err.println("LOGGER " + level + ": " + nameOfException);
+		} else {
+			System.err.println("LOGGER " + level + ": " + nameOfException + " (" + messageOfException + ")");
+		}
+	}
+
+	/**
+	 * Logs a caught exception with a custom text message.
+	 *
+	 * @param level
+	 *            Severity level
+	 * @param message
+	 *            Plain text message
+	 * @param exception
+	 *            Caught exception
+	 */
+	public static void log(final Level level, final Throwable exception, final String message) {
+		String nameOfException = exception.getClass().getName();
+		String messageOfException = exception.getMessage();
+
+		StringBuilder builder = new StringBuilder(BUFFER_SIZE);
+		builder.append(level);
+		builder.append(": ");
+		builder.append(message);
+		builder.append(" (");
+		builder.append(nameOfException);
+		if (messageOfException != null && !messageOfException.isEmpty()) {
+			builder.append(": ");
+			builder.append(messageOfException);
+		}
+		builder.append(")");
+
+		System.err.println(builder);
+	}
+
+}
---
+++ tinylog-api/src/test/java/org/tinylog/provider/InternalLoggerTest.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2016 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.provider;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.tinylog.Level;
+import org.tinylog.util.SystemStreamCollector;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link InternalLogger}.
+ */
+@RunWith(Parameterized.class)
+public final class InternalLoggerTest {
+
+	private Level severityLevel;
+	private String outputLevel;
+
+	private SystemStreamCollector systemStream;
+
+	/**
+	 * @param severityLevel
+	 *            Severity level for issuing log entry
+	 * @param outputLevel
+	 *            Expected severity level text in outputs
+	 */
+	public InternalLoggerTest(final Level severityLevel, final String outputLevel) {
+		this.severityLevel = severityLevel;
+		this.outputLevel = outputLevel;
+	}
+
+	/**
+	 * Returns all severity levels that are relevant for testing.
+	 *
+	 * @return Each object array contains the severity level for issuing log entries and the expected severity level
+	 *         text in outputs
+	 */
+	@Parameters(name = "{1}")
+	public static Collection<Object[]> getLevels() {
+		return asList(new Object[][] { { Level.WARNING, "WARNING" }, { Level.ERROR, "ERROR" } });
+	}
+
+	/**
+	 * Redirects {@link System#out} and {@link System#err}.
+	 */
+	@Before
+	public void init() {
+		systemStream = new SystemStreamCollector();
+	}
+
+	/**
+	 * Stops redirecting {@link System#out} and {@link System#err}.
+	 */
+	@After
+	public void reset() {
+		systemStream.close();
+
+		assertThat(systemStream.consumeStandardOutput()).isEmpty();
+		assertThat(systemStream.consumeErrorOutput()).isEmpty();
+	}
+
+	/**
+	 * Verifies that plain text messages will be output correctly.
+	 */
+	@Test
+	public void textMessage() {
+		InternalLogger.log(severityLevel, "Hello World!");
+
+		assertThat(systemStream.consumeErrorOutput())
+			.containsOnlyOnce(outputLevel)
+			.containsOnlyOnce("Hello World!")
+			.endsWith(System.lineSeparator())
+			.hasLineCount(1);
+	}
+
+	/**
+	 * Verifies that exceptions that don't provide a detail message will be output correctly.
+	 */
+	@Test
+	public void exception() {
+		InternalLogger.log(severityLevel, new NullPointerException());
+
+		assertThat(systemStream.consumeErrorOutput())
+			.containsOnlyOnce(outputLevel)
+			.containsOnlyOnce(NullPointerException.class.getName())
+			.endsWith(System.lineSeparator())
+			.hasLineCount(1);
+	}
+
+	/**
+	 * Verifies that exceptions that provide a detail message will be output correctly.
+	 */
+	@Test
+	public void exceptionWithDescription() {
+		InternalLogger.log(severityLevel, new IOException("File not found"));
+
+		assertThat(systemStream.consumeErrorOutput())
+			.containsOnlyOnce(outputLevel)
+			.containsOnlyOnce(IOException.class.getName())
+			.containsOnlyOnce("File not found")
+			.endsWith(System.lineSeparator())
+			.hasLineCount(1);
+	}
+
+	/**
+	 * Verifies that exceptions that don't provide a detail message will be output correctly in combination with a
+	 * custom text message.
+	 */
+	@Test
+	public void exceptionAndTextMessage() {
+		InternalLogger.log(severityLevel, new NullPointerException(), "Hello World!");
+
+		assertThat(systemStream.consumeErrorOutput())
+			.containsOnlyOnce(outputLevel)
+			.containsOnlyOnce(NullPointerException.class.getName())
+			.containsOnlyOnce("Hello World!")
+			.endsWith(System.lineSeparator())
+			.hasLineCount(1);
+	}
+
+	/**
+	 * Verifies that exceptions that provide a detail message will be output correctly in combination with a custom text
+	 * message.
+	 */
+	@Test
+	public void exceptionWithDescriptionAndTextMessage() {
+		InternalLogger.log(severityLevel, new IOException("File not found"), "Hello World!");
+
+		assertThat(systemStream.consumeErrorOutput())
+			.containsOnlyOnce(outputLevel)
+			.containsOnlyOnce(IOException.class.getName())
+			.containsOnlyOnce("File not found")
+			.containsOnlyOnce("Hello World!")
+			.endsWith(System.lineSeparator())
+			.hasLineCount(1);
+	}
+
+}
