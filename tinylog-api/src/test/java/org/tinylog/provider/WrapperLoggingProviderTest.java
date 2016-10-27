6ccc003134729528b4d72a65ca462be9075a6b33
Martin Winandy <martin.winandy@pmwmedia.de>
2016-10-27 22:35:45 +0200

    Add PowerMock

--- tinylog-api/src/test/java/org/tinylog/provider/WrapperLoggingProviderTest.java
+++ tinylog-api/src/test/java/org/tinylog/provider/WrapperLoggingProviderTest.java
@@ -18,9 +18,9 @@ import org.tinylog.Level;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
