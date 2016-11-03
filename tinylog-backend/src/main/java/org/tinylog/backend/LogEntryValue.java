90c9aa41a6dbcac50ab9be7db1a88ade584a87ea
Martin Winandy <martin.winandy@pmwmedia.de>
2016-11-03 20:11:21 +0100

    Add log entry value enum

---
+++ tinylog-backend/src/main/java/org/tinylog/backend/LogEntryValue.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2016 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.backend;
+
+/**
+ * Enumerated values for defining required log entry values.
+ */
+public enum LogEntryValue {
+
+	/**
+	 * Issuing date and time.
+	 *
+	 * @see LogEntry#getDate()
+	 */
+	DATE,
+
+	/**
+	 * Process ID of application.
+	 *
+	 * @see LogEntry#getProcessId()
+	 */
+	PROCESS_ID,
+
+	/**
+	 * Issuing thread.
+	 *
+	 * @see LogEntry#getThread()
+	 */
+	THREAD,
+
+	/**
+	 * Thread context mapping as it was active while issuing.
+	 *
+	 * @see LogEntry#getContext()
+	 */
+	CONTEXT,
+
+	/**
+	 * Name of issuing class.
+	 *
+	 * @see LogEntry#getClassName()
+	 */
+	CLASS,
+
+	/**
+	 * Name of issuing method.
+	 *
+	 * @see LogEntry#getMethodName()
+	 */
+	METHOD,
+
+	/**
+	 * Name of issuing source file.
+	 *
+	 * @see LogEntry#getFileName()
+	 */
+	FILE,
+
+	/**
+	 * Issuing line number in source file.
+	 *
+	 * @see LogEntry#getLineNumber()
+	 */
+	LINE,
+
+	/**
+	 * Tag from tagged logger.
+	 *
+	 * @see LogEntry#getTag()
+	 */
+	TAG,
+
+	/**
+	 * Severity level.
+	 *
+	 * @see LogEntry#getLevel()
+	 */
+	LEVEL,
+
+	/**
+	 * Text message.
+	 *
+	 * @see LogEntry#getMessage()
+	 */
+	MESSAGE,
+
+	/**
+	 * Catched exception or throwable.
+	 *
+	 * @see LogEntry#getException()
+	 */
+	EXCEPTION;
+
+}
