399a90d2ff9da2e37206c4cb257d8ed5980f8676
Martin Winandy <martin.winandy@pmwmedia.de>
2016-11-09 20:52:18 +0100

    Add alternative service loader implementation

---
+++ tinylog-api/src/main/java/org/tinylog/configuration/ServiceLoader.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright 2016 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.configuration;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.regex.Pattern;
+
+import org.tinylog.Level;
+import org.tinylog.provider.InternalLogger;
+
+/**
+ * Alternative service loader that supports constructors with arguments in opposite to {@link java.util.ServiceLoader}.
+ *
+ * @param <T>
+ *            Service interface
+ */
+public final class ServiceLoader<T> {
+
+	private static final String PREFIX = "META-INF/services/";
+
+	private static final Pattern splitPattern = Pattern.compile(" ");
+
+	private final Class<? extends T> service;
+	private final Class<?>[] argumentTypes;
+
+	private final ClassLoader classLoader;
+	private final Collection<String> classes;
+
+	/**
+	 * @param service
+	 *            Service interface
+	 * @param argumentTypes
+	 *            Expected argument types for constructors
+	 */
+	public ServiceLoader(final Class<? extends T> service, final Class<?>... argumentTypes) {
+		this.service = service;
+		this.argumentTypes = argumentTypes;
+		this.classLoader = Thread.currentThread().getContextClassLoader();
+		this.classes = loadClasses(service);
+	}
+
+	/**
+	 * Creates a defined service implementation. The name can be either the fully-qualified class name or the simplified
+	 * acronym. The acronym is the class name without package and service suffix.
+	 *
+	 * <p>
+	 * The acronym for <tt>org.tinylog.writers.RollingFileWriter</tt> is for example <tt>rolling file</tt>.
+	 * </p>
+	 *
+	 * @param name
+	 *            Acronym or class name of service implementation
+	 * @param arguments
+	 *            Arguments for constructor of service implementation
+	 * @return A new instance of service or {@code null} if failed to create service
+	 */
+	public T create(final String name, final Object... arguments) {
+		if (name.indexOf('.') == -1) {
+			String expectingClassName = toSimpleClassName(name);
+			for (String className : classes) {
+				int split = className.lastIndexOf('.');
+				String simpleClassName = split == -1 ? className : className.substring(split + 1);
+				if (expectingClassName.equals(simpleClassName)) {
+					return createInstance(className, arguments);
+				}
+			}
+
+			InternalLogger.log(Level.ERROR, "Service implementation '" + name + "' not found");
+			return null;
+		} else {
+			return createInstance(name, arguments);
+		}
+	}
+
+	/**
+	 * Creates all registered service implementations.
+	 *
+	 * @param arguments
+	 *            Arguments for constructors of service implementations
+	 * @return Instances of all service implementations
+	 */
+	public Collection<T> createAll(final Object... arguments) {
+		Collection<T> instances = new ArrayList<T>(classes.size());
+
+		for (String className : classes) {
+			T instance = createInstance(className, arguments);
+			if (instance != null) {
+				instances.add(instance);
+			}
+		}
+
+		return instances;
+	}
+
+	/**
+	 * Loads all registered service class names.
+	 *
+	 * @param <T>
+	 *            Service interface
+	 * @param service
+	 *            Service interface
+	 * @return Class names
+	 */
+	private static <T> Collection<String> loadClasses(final Class<? extends T> service) {
+		String name = PREFIX + service.getName();
+		Enumeration<URL> urls;
+		try {
+			urls = ClassLoader.getSystemResources(name);
+		} catch (IOException ex) {
+			InternalLogger.log(Level.ERROR, "Failed loading services from '" + name + "'");
+			return Collections.emptyList();
+		}
+
+		Collection<String> classes = new ArrayList<String>();
+
+		while (urls.hasMoreElements()) {
+			URL url = urls.nextElement();
+			BufferedReader reader = null;
+
+			try {
+				reader = new BufferedReader(new InputStreamReader(url.openStream(), "utf-8"));
+				for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+					line = line.trim();
+					if (line.length() > 0 && line.charAt(0) != '#' && !classes.contains(line)) {
+						classes.add(line);
+					}
+				}
+			} catch (IOException ex) {
+				InternalLogger.log(Level.ERROR, "Failed reading service resource '" + url + "'");
+			} finally {
+				if (reader != null) {
+					try {
+						reader.close();
+					} catch (IOException ex) {
+						// Ignore
+					}
+				}
+			}
+		}
+
+		return classes;
+	}
+
+	/**
+	 * Generates the simple class name from an acronym. A simple class name is the class name without package.
+	 *
+	 * <p>
+	 * The acronym <tt>rolling file</tt>, for example, will be transformed into <tt>RollingFileWriter</tt> for the
+	 * service interface <tt>Writer</tt>.
+	 * </p>
+	 *
+	 * @param name
+	 *            Simplified acronym
+	 * @return Simple class without package
+	 */
+	private String toSimpleClassName(final String name) {
+		StringBuilder builder = new StringBuilder(name.length());
+		for (String token : splitPattern.split(name)) {
+			if (!token.isEmpty()) {
+				builder.append(Character.toUpperCase(token.charAt(0)));
+				builder.append(token, 1, token.length());
+			}
+		}
+		builder.append(service.getSimpleName());
+		return builder.toString();
+	}
+
+	/**
+	 * Creates a new instance of a class.
+	 *
+	 * @param className
+	 *            Fully-qualified class name
+	 * @param arguments
+	 *            Arguments for constructor
+	 * @return A new instance of given class or {@code null} if creation failed
+	 */
+	@SuppressWarnings("unchecked")
+	private T createInstance(final String className, final Object... arguments) {
+		try {
+			Class<?> implementation = Class.forName(className, false, classLoader);
+			if (service.isAssignableFrom(implementation)) {
+				return (T) implementation.getDeclaredConstructor(argumentTypes).newInstance(arguments);
+			} else {
+				InternalLogger.log(Level.ERROR, "Class '" + className + "' does not implement servcie interface '" + service + "'");
+			}
+		} catch (ClassNotFoundException ex) {
+			InternalLogger.log(Level.ERROR, "Service implementation '" + className + "' not found");
+		} catch (NoSuchMethodException ex) {
+			InternalLogger.log(Level.ERROR, "Service implementation '" + className + "' has no matching constructor");
+		} catch (InstantiationException ex) {
+			InternalLogger.log(Level.ERROR, "Service implementation '" + className + "' is not instantiable");
+		} catch (IllegalAccessException ex) {
+			InternalLogger.log(Level.ERROR, "Constructor of service implementation '" + className + "' is not accessible");
+		} catch (IllegalArgumentException ex) {
+			InternalLogger.log(Level.ERROR, "Illegal arguments for constructor of service implementation '" + className + "'");
+		} catch (InvocationTargetException ex) {
+			InternalLogger.log(Level.ERROR, ex.getTargetException(), "Failed creating service implementation '" + className + "'");
+		}
+
+		return null;
+	}
+
+}
---
+++ tinylog-api/src/test/java/org/tinylog/configuration/ServiceLoaderTest.java
@@ -0,0 +1,307 @@
+/*
+ * Copyright 2016 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.configuration;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.tinylog.rules.SystemStreamCollector;
+import org.tinylog.util.FileSystem;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyNew;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+
+/**
+ * Tests for {@link ServiceLoader}.
+ */
+@RunWith(PowerMockRunner.class)
+public final class ServiceLoaderTest {
+
+	private static final String SERVICE_PREFIX = "META-INF/services/";
+
+	/**
+	 * Redirects and collects system output streams.
+	 */
+	@Rule
+	public final SystemStreamCollector systemStream = new SystemStreamCollector(true);
+
+	/**
+	 * Creates pre-filled service file for {@link List} in class path.
+	 *
+	 * @throws Exception
+	 *             Failed creating service file
+	 */
+	@Before
+	public void init() throws Exception {
+		FileSystem.createResource(SERVICE_PREFIX + List.class.getName(), ArrayList.class.getName(), LinkedList.class.getName());
+	}
+
+	/**
+	 * Deletes created service file from class path.
+	 *
+	 * @throws Exception
+	 *             Failed deleting service file
+	 */
+	@After
+	public void clear() throws Exception {
+		FileSystem.deleteResource(SERVICE_PREFIX + List.class.getName());
+	}
+
+	/**
+	 * Verifies that service loader works if there are no registered services.
+	 *
+	 * @throws Exception
+	 *             Failed deleting service file
+	 */
+	@Test
+	public void noRegisteredServices() throws Exception {
+		FileSystem.deleteResource(SERVICE_PREFIX + List.class.getName());
+
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class);
+		assertThat(loader.createAll()).isEmpty();
+	}
+
+	/**
+	 * Verifies that service implementations can be found by acronym.
+	 */
+	@Test
+	public void getServiceByAcronym() {
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class);
+		assertThat(loader.create("array")).isInstanceOf(ArrayList.class);
+		assertThat(loader.create("linked")).isInstanceOf(LinkedList.class);
+	}
+
+	/**
+	 * Verifies that service implementations can be found by class name.
+	 */
+	@Test
+	public void getServiceByClassName() {
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class);
+		assertThat(loader.create(ArrayList.class.getName())).isInstanceOf(ArrayList.class);
+		assertThat(loader.create(LinkedList.class.getName())).isInstanceOf(LinkedList.class);
+	}
+
+	/**
+	 * Verifies that service loader returns an instance for all registered services.
+	 */
+	@Test
+	public void getAllServices() {
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class);
+		assertThat(loader.createAll())
+			.hasSize(2)
+			.hasAtLeastOneElementOfType(ArrayList.class)
+			.hasAtLeastOneElementOfType(LinkedList.class);
+	}
+
+	/**
+	 * Verifies that service files with empty lines can be parsed without any errors.
+	 *
+	 * @throws Exception
+	 *             Failed overriding service file
+	 */
+	@Test
+	public void emptyLines() throws Exception {
+		FileSystem.createResource(SERVICE_PREFIX + List.class.getName(), "", ArrayList.class.getName(), "");
+
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class);
+		assertThat(loader.createAll())
+			.hasSize(1)
+			.hasAtLeastOneElementOfType(ArrayList.class);
+	}
+
+	/**
+	 * Verifies that service files with comments can be parsed without any errors.
+	 *
+	 * @throws Exception
+	 *             Failed overriding service file
+	 */
+	@Test
+	public void comments() throws Exception {
+		FileSystem.createResource(SERVICE_PREFIX + List.class.getName(), "#Comment", ArrayList.class.getName());
+
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class);
+		assertThat(loader.createAll())
+			.hasSize(1)
+			.hasAtLeastOneElementOfType(ArrayList.class);
+	}
+
+	/**
+	 * Verifies that an accurate error message will be output, if there is no registered service for an acronym.
+	 */
+	@Test
+	public void invalidAcronym() {
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class);
+		assertThat(loader.create("test")).isNull();
+		assertThat(systemStream.consumeErrorOutput()).contains("ERROR").containsOnlyOnce("test");
+	}
+
+	/**
+	 * Verifies that an accurate error message will be output, if a service implementation class does not exist.
+	 */
+	@Test
+	public void nonExistentClassName() {
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class);
+		assertThat(loader.create("my.package.Test")).isNull();
+		assertThat(systemStream.consumeErrorOutput()).contains("ERROR").containsOnlyOnce("my.package.Test");
+	}
+
+	/**
+	 * Verifies that an accurate error message will be output, if a service implementation class does not implement the
+	 * required service interface.
+	 */
+	@Test
+	public void notImplementingInterface() {
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class);
+		assertThat(loader.create(HashSet.class.getName())).isNull();
+
+		assertThat(systemStream.consumeErrorOutput())
+			.contains("ERROR")
+			.containsOnlyOnce(HashSet.class.getName())
+			.containsOnlyOnce(List.class.getName());
+	}
+
+	/**
+	 * Verifies that an accurate error message will be output, if a service implementation class is abstract.
+	 */
+	@Test
+	public void abstractServiceClass() {
+		String className = AbstractListWithPublicConstructor.class.getName();
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class);
+
+		assertThat(loader.create(className)).isNull();
+		assertThat(systemStream.consumeErrorOutput()).contains("ERROR").containsOnlyOnce(className);
+	}
+
+	/**
+	 * Verifies that an accurate error message will be output, if a service implementation has no matching constructor
+	 * for the passed arguments.
+	 */
+	@Test
+	public void withoutMatchingConstructor() {
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class, int.class);
+		assertThat(loader.create(LinkedList.class.getName(), 1)).isNull();
+		assertThat(systemStream.consumeErrorOutput()).contains("ERROR").containsOnlyOnce(LinkedList.class.getName());
+	}
+
+	/**
+	 * Verifies that an accurate error message will be output, if a constructor of a service implementation is not
+	 * public.
+	 */
+	@Test
+	public void invisibleConstructor() {
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class);
+		assertThat(loader.create(Collections.EMPTY_LIST.getClass().getName())).isNull();
+		assertThat(systemStream.consumeErrorOutput()).contains("ERROR").containsOnlyOnce(Collections.EMPTY_LIST.getClass().getName());
+	}
+
+	/**
+	 * Verifies that an accurate error message will be output, if the number of passed arguments is not equal to the
+	 * number of specified arguments.
+	 */
+	@Test
+	public void wrongNumberOfArguments() {
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class, int.class);
+		assertThat(loader.create(ArrayList.class.getName())).isNull();
+		assertThat(systemStream.consumeErrorOutput()).contains("ERROR").containsOnlyOnce(ArrayList.class.getName());
+	}
+
+	/**
+	 * Verifies that an accurate error message will be output, if an argument has not the expected type.
+	 */
+	@Test
+	public void invalidArgumentType() {
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class, int.class);
+		assertThat(loader.create(ArrayList.class.getName(), "test")).isNull();
+		assertThat(systemStream.consumeErrorOutput()).contains("ERROR").containsOnlyOnce(ArrayList.class.getName());
+	}
+
+	/**
+	 * Verifies that an accurate error message will be output, if an argument has an invalid value.
+	 */
+	@Test
+	public void invalidArgumentValue() {
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class, int.class);
+		assertThat(loader.create(ArrayList.class.getName(), -1)).isNull();
+		assertThat(systemStream.consumeErrorOutput()).contains("ERROR").containsOnlyOnce(ArrayList.class.getName());
+	}
+
+	/**
+	 * Verifies that an accurate error message will be output, if service files can be not loaded as resources.
+	 *
+	 * @throws IOException
+	 *             Failed invoking {@code getSystemResources()}
+	 */
+	@Test
+	@PrepareForTest(ServiceLoader.class)
+	public void loadingServiceFilesFails() throws IOException {
+		mockStatic(ClassLoader.class);
+		when(ClassLoader.getSystemResources(anyString())).thenThrow(new IOException());
+
+		ServiceLoader<?> loader = new ServiceLoader<>(List.class);
+		assertThat(loader.createAll()).isEmpty();
+		assertThat(systemStream.consumeErrorOutput()).contains("ERROR").containsOnlyOnce(SERVICE_PREFIX + List.class.getName());
+	}
+
+	/**
+	 * Verifies that an accurate error message will be output, if a service file can be not open for reading.
+	 *
+	 * @throws Exception
+	 *             Failed mocking reader
+	 */
+	@Test
+	@PrepareForTest(ServiceLoader.class)
+	public void readingServiceFileFails() throws Exception {
+		whenNew(BufferedReader.class).withAnyArguments().thenThrow(new IOException());
+		try {
+			ServiceLoader<?> loader = new ServiceLoader<>(List.class);
+			assertThat(loader.createAll()).isEmpty();
+			assertThat(systemStream.consumeErrorOutput()).contains("ERROR").containsOnlyOnce(SERVICE_PREFIX + List.class.getName());
+		} finally {
+			ArgumentCaptor<Reader> captor = ArgumentCaptor.forClass(Reader.class);
+			verifyNew(BufferedReader.class).withArguments(captor.capture());
+			captor.getValue().close();
+		}
+	}
+
+	/**
+	 * Dummy class for testing to load an abstract service class with visible constructor.
+	 */
+	public abstract static class AbstractListWithPublicConstructor extends AbstractList<Object> {
+
+		/** */
+		public AbstractListWithPublicConstructor() {
+		}
+
+	}
+
+}
