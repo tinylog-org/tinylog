3ce6cae93705d4733d315969ffbdf85a4ae0d48b
Martin Winandy <martin.winandy@pmwmedia.de>
2016-11-21 20:25:19 +0100

    Add file writer

---
+++ tinylog-backend/src/main/java/org/tinylog/writers/FileWriter.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2016 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.writers;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Map;
+
+import org.tinylog.writers.raw.ByteArrayWriter;
+
+/**
+ * Writer to output log entries to a log file. Already existing files can be continued and the output can be buffered
+ * for improving performance.
+ */
+public final class FileWriter extends AbstractFormatPatternWriter {
+
+	private final Charset charset;
+	private ByteArrayWriter writer;
+
+	/**
+	 * @param properties
+	 *            Configuration for writer
+	 *
+	 * @throws FileNotFoundException
+	 *             Log file does not exist or cannot be opened for any other reason
+	 * @throws IllegalArgumentException
+	 *             Log file is not defined in configuration
+	 */
+	public FileWriter(final Map<String, String> properties) throws FileNotFoundException {
+		super(properties);
+
+		String fileName = getFileName(properties);
+		boolean append = Boolean.parseBoolean(properties.get("append"));
+		boolean buffered = Boolean.parseBoolean(properties.get("buffered"));
+		boolean writingThread = Boolean.parseBoolean(properties.get("writingThread"));
+
+		charset = getCharset(properties);
+		writer = createByteArrayWriter(fileName, append, buffered, !writingThread);
+	}
+
+	@Override
+	public void flush() throws IOException {
+		writer.flush();
+	}
+
+	@Override
+	public void close() throws IOException {
+		writer.close();
+	}
+
+	@Override
+	protected void write(final String logEntry) throws IOException {
+		byte[] data = logEntry.getBytes(charset);
+		writer.write(data, data.length);
+	}
+
+}
---
+++ tinylog-backend/src/test/java/org/tinylog/writers/FileWriterTest.java
@@ -0,0 +1,222 @@
+/*
+ * Copyright 2016 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.writers;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.powermock.reflect.Whitebox;
+import org.tinylog.backend.LogEntryValue;
+import org.tinylog.backend.util.LogEntryBuilder;
+import org.tinylog.rules.SystemStreamCollector;
+import org.tinylog.util.FileSystem;
+import org.tinylog.writers.raw.ByteArrayWriter;
+import org.tinylog.writers.raw.SynchronizedWriterDecorator;
+
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.singletonMap;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.tinylog.util.Maps.doubletonMap;
+import static org.tinylog.util.Maps.tripletonMap;
+
+/**
+ * Tests for {@link FileWriter}.
+ */
+public final class FileWriterTest {
+
+	private static final String NEW_LINE = System.getProperty("line.separator");
+
+	/**
+	 * Redirects and collects system output streams.
+	 */
+	@Rule
+	public final SystemStreamCollector systemStream = new SystemStreamCollector(true);
+
+	/**
+	 * Verifies that log entries will be immediately output, if buffer is disabled.
+	 *
+	 * @throws Exception
+	 *             Failed writing to file
+	 */
+	@Test
+	public void unbufferedWriting() throws Exception {
+		String file = FileSystem.createTemporaryFile();
+		FileWriter writer = new FileWriter(tripletonMap("file", file, "format", "{message}", "buffered", "false"));
+
+		writer.write(LogEntryBuilder.empty().message("Hello World!").create());
+		assertThat(FileSystem.readFile(file)).isEqualTo("Hello World!" + NEW_LINE);
+
+		writer.close();
+		assertThat(FileSystem.readFile(file)).isEqualTo("Hello World!" + NEW_LINE);
+	}
+
+	/**
+	 * Verifies that log entries will be output after flushing, if buffer is enabled.
+	 *
+	 * @throws Exception
+	 *             Failed writing to file
+	 */
+	@Test
+	public void bufferedWriting() throws Exception {
+		String file = FileSystem.createTemporaryFile();
+		FileWriter writer = new FileWriter(tripletonMap("file", file, "format", "{message}", "buffered", "true"));
+
+		writer.write(LogEntryBuilder.empty().message("Hello World!").create());
+		assertThat(FileSystem.readFile(file)).isEmpty();
+
+		writer.flush();
+		assertThat(FileSystem.readFile(file)).isEqualTo("Hello World!" + NEW_LINE);
+
+		writer.close();
+		assertThat(FileSystem.readFile(file)).isEqualTo("Hello World!" + NEW_LINE);
+	}
+
+	/**
+	 * Verifies that an already existing file will be overridden, if append mode is disabled.
+	 *
+	 * @throws Exception
+	 *             Failed writing to file
+	 */
+	@Test
+	public void appendingDisabled() throws Exception {
+		String file = FileSystem.createTemporaryFile("Test");
+
+		FileWriter writer = new FileWriter(tripletonMap("file", file, "format", "{message}", "append", "false"));
+		writer.write(LogEntryBuilder.empty().message("Hello World!").create());
+		writer.close();
+
+		assertThat(FileSystem.readFile(file)).isEqualTo("Hello World!" + NEW_LINE);
+	}
+
+	/**
+	 * Verifies that an already existing file will be continued, if append mode is enabled.
+	 *
+	 * @throws Exception
+	 *             Failed writing to file
+	 */
+	@Test
+	public void appendingEnabled() throws Exception {
+		String file = FileSystem.createTemporaryFile("Test");
+
+		FileWriter writer = new FileWriter(tripletonMap("file", file, "format", "{message}", "append", "true"));
+		writer.write(LogEntryBuilder.empty().message("Hello World!").create());
+		writer.close();
+
+		assertThat(FileSystem.readFile(file)).isEqualTo("Test" + NEW_LINE + "Hello World!" + NEW_LINE);
+	}
+
+	/**
+	 * Verifies that writing works and underlying byte array writer is thread-safe, if writing thread is disable.
+	 *
+	 * @throws Exception
+	 *             Failed writing to file
+	 */
+	@Test
+	public void writingThreadDisabled() throws Exception {
+		String file = FileSystem.createTemporaryFile();
+		FileWriter writer = new FileWriter(tripletonMap("file", file, "format", "{message}", "writingThread", "false"));
+
+		writer.write(LogEntryBuilder.empty().message("Hello World!").create());
+		assertThat(FileSystem.readFile(file)).isEqualTo("Hello World!" + NEW_LINE);
+		writer.close();
+
+		assertThat(Whitebox.getInternalState(writer, ByteArrayWriter.class)).isInstanceOf(SynchronizedWriterDecorator.class);
+	}
+
+	/**
+	 * Verifies that writing works and underlying byte array writer is not thread-safe, if writing thread is enabled.
+	 *
+	 * @throws Exception
+	 *             Failed writing to file
+	 */
+	@Test
+	public void writingThreadEnabled() throws Exception {
+		String file = FileSystem.createTemporaryFile();
+		FileWriter writer = new FileWriter(tripletonMap("file", file, "format", "{message}", "writingThread", "true"));
+
+		writer.write(LogEntryBuilder.empty().message("Hello World!").create());
+		assertThat(FileSystem.readFile(file)).isEqualTo("Hello World!" + NEW_LINE);
+		writer.close();
+
+		assertThat(Whitebox.getInternalState(writer, ByteArrayWriter.class)).isNotInstanceOf(SynchronizedWriterDecorator.class);
+	}
+
+	/**
+	 * Verifies that a configured charset will be used for encoding text.
+	 *
+	 * @throws Exception
+	 *             Failed writing to file
+	 */
+	@Test
+	public void definedCharset() throws Exception {
+		String file = FileSystem.createTemporaryFile();
+
+		FileWriter writer = new FileWriter(tripletonMap("file", file, "format", "{message}", "charset", "UTF-16"));
+		writer.write(LogEntryBuilder.empty().message("Hello World!").create());
+		writer.close();
+
+		assertThat(FileSystem.readFile(file, StandardCharsets.UTF_16)).isEqualTo("Hello World!" + NEW_LINE);
+	}
+
+	/**
+	 * Verifies that the default pattern contains a minimum set of informations.
+	 *
+	 * @throws Exception
+	 *             Failed writing to file
+	 */
+	@Test
+	public void defaultFormatPattern() throws Exception {
+		String file = FileSystem.createTemporaryFile();
+		FileWriter writer = new FileWriter(singletonMap("file", file));
+
+		assertThat(writer.getRequiredLogEntryValues())
+			.contains(LogEntryValue.DATE, LogEntryValue.LEVEL, LogEntryValue.MESSAGE, LogEntryValue.EXCEPTION);
+
+		writer.write(LogEntryBuilder.prefilled(FileWriterTest.class).create());
+		writer.close();
+
+		assertThat(FileSystem.readFile(file))
+			.contains("1985").contains("03")
+			.contains("TRACE")
+			.contains("Hello World!")
+			.endsWith(NEW_LINE);
+	}
+
+	/**
+	 * Verifies that an exception will be thrown, if no file name is defined. The message of the thrown exception should
+	 * contain "file name" or "filename".
+	 */
+	@Test
+	public void missingFileName() {
+		assertThatThrownBy(() -> new FileWriter(emptyMap())).hasMessageMatching("(?i).*file ?name.*");
+	}
+
+	/**
+	 * Verifies that an invalid charset will be reported as error.
+	 *
+	 * @throws IOException
+	 *             Failed opening file
+	 */
+	@Test
+	public void invalidCharset() throws IOException {
+		String file = FileSystem.createTemporaryFile();
+		new FileWriter(doubletonMap("file", file, "charset", "UTF-42")).close();
+
+		assertThat(systemStream.consumeErrorOutput()).containsOnlyOnce("ERROR").containsOnlyOnce("charset").containsOnlyOnce("UTF-42");
+	}
+
+}
