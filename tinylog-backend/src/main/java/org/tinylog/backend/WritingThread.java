4ec01c1a658909bf66ee1edf6fab71351c8db06b
Martin Winandy <martin.winandy@pmwmedia.de>
2016-11-24 20:32:29 +0100

    Add thread for writing log entries asynchronously

---
+++ tinylog-backend/src/main/java/org/tinylog/backend/WritingThread.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright 2016 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.backend;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.tinylog.Level;
+import org.tinylog.provider.InternalLogger;
+import org.tinylog.writers.Writer;
+
+/**
+ * Thread for writing log entries asynchronously.
+ */
+final class WritingThread extends Thread {
+
+	private static final String THREAD_NAME = "tinylog-WritingThread";
+	private static final long MILLISECONDS_TO_SLEEP = 10L;
+
+	private final Object mutex;
+	private final Collection<Writer> writers;
+	private List<Task> tasks;
+
+	/**
+	 * @param writers
+	 *            Open writers
+	 */
+	WritingThread(final Collection<Writer> writers) {
+		this.mutex = new Object();
+		this.writers = writers;
+		this.tasks = new ArrayList<Task>();
+
+		setName(THREAD_NAME);
+		setPriority(Thread.MIN_PRIORITY);
+		setDaemon(true);
+	}
+
+	/**
+	 * Fetches log entries and writes them until receiving a poison task.
+	 */
+	@Override
+	public void run() {
+		Collection<Writer> writers = new ArrayList<Writer>(1);
+
+		while (true) {
+			for (Task task : receiveTasks()) {
+				if (task == Task.POISON) {
+					close();
+					return;
+				} else {
+					write(writers, task);
+				}
+			}
+
+			flush(writers);
+			writers.clear();
+
+			try {
+				sleep(MILLISECONDS_TO_SLEEP);
+			} catch (InterruptedException ex) {
+				// Ignore and continue
+			}
+		}
+	}
+
+	/**
+	 * Adds a log entry for writing.
+	 *
+	 * @param writer
+	 *            Writer to write given log entry
+	 * @param logEntry
+	 *            Log entry to write
+	 */
+	void add(final Writer writer, final LogEntry logEntry) {
+		Task task = new Task(writer, logEntry);
+		synchronized (mutex) {
+			tasks.add(task);
+		}
+	}
+
+	/**
+	 * Shuts this writing thread down after writing all already added log entries.
+	 *
+	 * <p>
+	 * This method doesn't wait until the writing thread is down. {@link Thread#join()} can be used afterwards to wait
+	 * for termination.
+	 * </p>
+	 */
+	void shutdown() {
+		synchronized (mutex) {
+			tasks.add(Task.POISON);
+		}
+
+		interrupt();
+	}
+
+	/**
+	 * Receives all added log entries.
+	 *
+	 * @return Log entries to write
+	 */
+	private List<Task> receiveTasks() {
+		synchronized (mutex) {
+			if (tasks.isEmpty()) {
+				return Collections.emptyList();
+			} else {
+				List<Task> currentTasks = tasks;
+				tasks = new ArrayList<Task>();
+				return currentTasks;
+			}
+		}
+	}
+
+	/**
+	 * Writes a log entry.
+	 *
+	 * @param writers
+	 *            Mutable collection of used writers
+	 * @param task
+	 *            Log entry to write
+	 */
+	private void write(final Collection<Writer> writers, final Task task) {
+		try {
+			Writer writer = task.writer;
+			writer.write(task.logEntry);
+			if (!writers.contains(writer)) {
+				writers.add(writer);
+			}
+		} catch (Exception ex) {
+			InternalLogger.log(Level.ERROR, ex, "Failed to write log entry");
+		}
+	}
+
+	/**
+	 * Flushes a collection of writers.
+	 *
+	 * @param writers
+	 *            Writers to flush
+	 */
+	private void flush(final Collection<Writer> writers) {
+		for (Writer writer : writers) {
+			try {
+				writer.flush();
+			} catch (Exception ex) {
+				InternalLogger.log(Level.ERROR, ex, "Failed to flush writer");
+			}
+		}
+	}
+
+	/**
+	 * Closes all writers.
+	 */
+	private void close() {
+		for (Writer writer : writers) {
+			try {
+				writer.close();
+			} catch (Exception ex) {
+				InternalLogger.log(Level.ERROR, ex, "Failed to close writer");
+			}
+		}
+	}
+
+	/**
+	 * Tuple that contains a log entry and a writer for outputting it.
+	 */
+	private static final class Task {
+
+		private static final Task POISON = null;
+
+		private final Writer writer;
+		private final LogEntry logEntry;
+
+		/**
+		 * @param writer
+		 *            Writer for outputting log entry
+		 * @param logEntry
+		 *            Log entry to write
+		 */
+		Task(final Writer writer, final LogEntry logEntry) {
+			this.writer = writer;
+			this.logEntry = logEntry;
+		}
+
+	}
+
+}
---
+++ tinylog-backend/src/test/java/org/tinylog/backend/WritingThreadTest.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright 2016 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.backend;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.tinylog.backend.util.LogEntryBuilder;
+import org.tinylog.rules.SystemStreamCollector;
+import org.tinylog.util.FileSystem;
+import org.tinylog.writers.FileWriter;
+import org.tinylog.writers.Writer;
+
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.mock;
+
+/**
+ * Tests for {@link WritingThread}.
+ */
+public final class WritingThreadTest {
+
+	/**
+	 * Redirects and collects system output streams.
+	 */
+	@Rule
+	public final SystemStreamCollector systemStream = new SystemStreamCollector(true);
+
+	/**
+	 * Verifies that a single log entry will be written.
+	 *
+	 * @throws Exception
+	 *             Illegal thread or mock invocation
+	 */
+	@Test
+	public void singleLogEntry() throws Exception {
+		Writer writer = mock(Writer.class);
+		LogEntry entry = LogEntryBuilder.empty().create();
+
+		WritingThread thread = new WritingThread(singletonList(writer));
+		thread.start();
+
+		thread.add(writer, entry);
+		Thread.sleep(100); // Wait for flushing
+
+		thread.shutdown();
+		thread.join();
+
+		verify(writer).write(entry);
+		verify(writer).flush();
+		verify(writer).close();
+	}
+
+	/**
+	 * Verifies that multiple threads can serve log entries simultaneously.
+	 *
+	 * @throws IOException
+	 *             Failed creating or opening log file
+	 * @throws InterruptedException
+	 *             Failed waiting for a thread
+	 */
+	@Test
+	public void multiThreaded() throws IOException, InterruptedException {
+		String file = FileSystem.createTemporaryFile();
+		Map<String, String> configuration = new HashMap<>();
+		configuration.put("file", file);
+		configuration.put("buffered", "true");
+		configuration.put("format", "{message}");
+		configuration.put("writingThread", "true");
+
+		Writer writer = new FileWriter(configuration);
+		LogEntry entry = LogEntryBuilder.empty().message("Hello World!").create();
+
+		WritingThread writingThread = new WritingThread(singletonList(writer));
+		writingThread.start();
+
+		List<Thread> threads = new ArrayList<>();
+		for (int i = 0; i < 10; ++i) {
+			threads.add(new Thread(() -> {
+				for (int j = 0; j < 1000; ++j) {
+					writingThread.add(writer, entry);
+				}
+			}));
+		}
+
+		threads.forEach(thread -> thread.start());
+
+		for (Thread thread : threads) {
+			thread.join();
+		}
+
+		writingThread.shutdown();
+		writingThread.join();
+
+		assertThat(Files.readAllLines(Paths.get(file))).hasSize(10 * 1000).containsOnly("Hello World!");
+	}
+
+	/**
+	 * Verifies that a thrown exception will be reported while writing.
+	 *
+	 * @throws Exception
+	 *             Illegal thread or mock invocation
+	 */
+	@Test
+	public void failWriting() throws Exception {
+		Writer writer = mock(Writer.class);
+		doThrow(IOException.class).when(writer).write(any());
+		LogEntry entry = LogEntryBuilder.empty().create();
+
+		WritingThread thread = new WritingThread(singletonList(writer));
+		thread.start();
+		thread.add(writer, entry);
+		thread.shutdown();
+		thread.join();
+
+		verify(writer).write(entry);
+		assertThat(systemStream.consumeErrorOutput()).containsOnlyOnce("ERROR").containsOnlyOnce(IOException.class.getName());
+		verify(writer).close();
+	}
+
+	/**
+	 * Verifies that a thrown exception will be reported while flushing an underlying writer.
+	 *
+	 * @throws Exception
+	 *             Illegal thread or mock invocation
+	 */
+	@Test
+	public void failFlushing() throws Exception {
+		Writer writer = mock(Writer.class);
+		doThrow(IOException.class).when(writer).flush();
+
+		WritingThread thread = new WritingThread(singletonList(writer));
+		thread.start();
+
+		thread.add(writer, LogEntryBuilder.empty().create());
+		Thread.sleep(100); // Wait for flushing
+
+		thread.shutdown();
+		thread.join();
+
+		verify(writer).flush();
+		assertThat(systemStream.consumeErrorOutput()).containsOnlyOnce("ERROR").containsOnlyOnce(IOException.class.getName());
+		verify(writer).close();
+	}
+
+	/**
+	 * Verifies that a thrown exception will be reported while closing an underlying writer.
+	 *
+	 * @throws Exception
+	 *             Illegal thread or mock invocation
+	 */
+	@Test
+	public void failClosing() throws Exception {
+		Writer writer = mock(Writer.class);
+		doThrow(IOException.class).when(writer).close();
+
+		WritingThread thread = new WritingThread(singletonList(writer));
+		thread.start();
+		thread.shutdown();
+		thread.join();
+
+		verify(writer).close();
+		assertThat(systemStream.consumeErrorOutput()).containsOnlyOnce("ERROR").containsOnlyOnce(IOException.class.getName());
+	}
+
+}
