2c4223f78a15f64e23c97f992abac555e76438ea
Martin Winandy <martin.winandy@pmwmedia.de>
2018-03-11 15:21:31 +0100

    Add a timestamp formatter for encapsulating simple date format

---
+++ tinylog-api/src/main/java/org/tinylog/runtime/FastTimestampFormatter.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2018 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.runtime;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Thread-safe formatter that based on {@link SimpleDateFormat} with millisecond precision. The last formatted timestamp
+ * will be cached.
+ */
+final class FastTimestampFormatter implements TimestampFormatter {
+
+	private static final int MILLISECOND_PRECISION = 1;
+	private static final int SECOND_PRECISION = 1000;
+	private static final int MINUTE_PRECISION = 60000;
+
+	private final DateFormat formatter;
+	private final long divisor;
+
+	private Date lastDate;
+	private String lastFormat;
+
+	/**
+	 * @param pattern
+	 *            Format pattern that is compatible with {@link SimpleDateFormat}
+	 * @param locale
+	 *            Locale for formatting
+	 */
+	FastTimestampFormatter(final String pattern, final Locale locale) {
+		formatter = new SimpleDateFormat(pattern, locale);
+		divisor = pattern.contains("S") ? MILLISECOND_PRECISION : pattern.contains("s") ? SECOND_PRECISION : MINUTE_PRECISION;
+	}
+
+	@Override
+	public String format(final Timestamp timestamp) {
+		return format(timestamp.toDate());
+	}
+
+	/**
+	 * Formats a legacy {@link Date}.
+	 *
+	 * @param date
+	 *            Date to format
+	 * @return Formatted date
+	 */
+	private String format(final Date date) {
+		synchronized (formatter) {
+			if (lastDate == null || date.getTime() / divisor != lastDate.getTime() / divisor) {
+				lastDate = date;
+				lastFormat = formatter.format(date);
+			}
+			return lastFormat;
+		}
+	}
+
+}
---
+++ tinylog-api/src/test/java/org/tinylog/runtime/FastTimestampFormatterTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2018 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.runtime;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Locale;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+/**
+ * Tests for {@link FastTimestampFormatter}.
+ */
+public final class FastTimestampFormatterTest {
+
+	/**
+	 * Verifies that timestamps can be formatted minute precise.
+	 */
+	@Test
+	public void minutePrecision() {
+		FastTimestampFormatter formatter = new FastTimestampFormatter("yyyy-MM-dd hh:mm", Locale.US);
+
+		assertThat(format(LocalDateTime.of(2016, 02, 01, 12, 30, 55, 000_000_000), formatter)).isEqualTo("2016-02-01 12:30");
+		assertThat(format(LocalDateTime.of(2016, 02, 01, 12, 30, 55, 999_000_000), formatter)).isEqualTo("2016-02-01 12:30");
+		assertThat(format(LocalDateTime.of(2016, 02, 01, 12, 30, 56, 000_000_000), formatter)).isEqualTo("2016-02-01 12:30");
+	}
+
+	/**
+	 * Verifies that timestamps can be formatted second precise.
+	 */
+	@Test
+	public void secondPrecision() {
+		FastTimestampFormatter formatter = new FastTimestampFormatter("yyyy-MM-dd hh:mm:ss", Locale.US);
+
+		assertThat(format(LocalDateTime.of(2016, 02, 01, 12, 30, 55, 000_000_000), formatter)).isEqualTo("2016-02-01 12:30:55");
+		assertThat(format(LocalDateTime.of(2016, 02, 01, 12, 30, 55, 999_000_000), formatter)).isEqualTo("2016-02-01 12:30:55");
+		assertThat(format(LocalDateTime.of(2016, 02, 01, 12, 30, 56, 000_000_000), formatter)).isEqualTo("2016-02-01 12:30:56");
+	}
+
+	/**
+	 * Verifies that timestamps can be formatted millisecond precise.
+	 */
+	@Test
+	public void milliseondPrecision() {
+		FastTimestampFormatter formatter = new FastTimestampFormatter("hh:mm:ss.SSS", Locale.US);
+
+		assertThat(format(LocalDateTime.of(2016, 02, 01, 12, 30, 55, 000_000_000), formatter)).isEqualTo("12:30:55.000");
+		assertThat(format(LocalDateTime.of(2016, 02, 01, 12, 30, 55, 999_000_000), formatter)).isEqualTo("12:30:55.999");
+		assertThat(format(LocalDateTime.of(2016, 02, 01, 12, 30, 56, 000_000_000), formatter)).isEqualTo("12:30:56.000");
+	}
+
+	/**
+	 * Formats a {@link LocalDateTime} by using a given formatter.
+	 *
+	 * @param date
+	 *            Date to format
+	 * @param formatter
+	 *            Formatter for formatting the given date
+	 * @return Formatted date
+	 */
+	private String format(final LocalDateTime date, final FastTimestampFormatter formatter) {
+		Instant instant = date.atZone(ZoneId.systemDefault()).toInstant();
+
+		Timestamp timestamp = mock(Timestamp.class);
+		when(timestamp.toDate()).thenReturn(Date.from(instant));
+
+		return formatter.format(timestamp);
+	}
+
+}
