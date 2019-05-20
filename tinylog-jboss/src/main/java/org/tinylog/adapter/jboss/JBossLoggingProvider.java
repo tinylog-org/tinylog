1300ca9807cc80730dc479144c83d47146e95e11
Martin Winandy <martin.winandy@pmwmedia.de>
2019-05-20 22:36:53 +0200

    Add logging provider for forwarding log entries to JBoss Logging

---
+++ tinylog-jboss/src/main/java/org/tinylog/adapter/jboss/JBossLoggingProvider.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2019 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.adapter.jboss;
+
+import java.util.Locale;
+
+import org.jboss.logging.Logger;
+import org.tinylog.Level;
+import org.tinylog.provider.ContextProvider;
+import org.tinylog.provider.LoggingProvider;
+import org.tinylog.provider.MessageFormatter;
+import org.tinylog.runtime.RuntimeProvider;
+
+/**
+ * Logging provider that forwards all log entries to JBoss Logging.
+ */
+public final class JBossLoggingProvider implements LoggingProvider {
+
+	private ContextProvider contextProvider;
+	private MessageFormatter formatter;
+
+	/** */
+	public JBossLoggingProvider() {
+		contextProvider = new JBossContextProvider();
+		formatter = new MessageFormatter(Locale.getDefault());
+	}
+
+	@Override
+	public ContextProvider getContextProvider() {
+		return contextProvider;
+	}
+
+	@Override
+	public Level getMinimumLevel() {
+		return Level.TRACE;
+	}
+
+	@Override
+	public Level getMinimumLevel(final String tag) {
+		return Level.TRACE;
+	}
+
+	@Override
+	public boolean isEnabled(final int depth, final String tag, final Level level) {
+		String callerClassName = RuntimeProvider.getCallerClassName(depth);
+		return Logger.getLogger(callerClassName).isEnabled(translate(level));
+	}
+
+	@Override
+	public void log(final int depth, final String tag, final Level level, final Throwable exception, final Object obj,
+		final Object... arguments) {
+		String callerClassName = RuntimeProvider.getCallerClassName(depth);
+		Logger jbossLogger = Logger.getLogger(callerClassName);
+		org.jboss.logging.Logger.Level jbossLevel = translate(level);
+
+		if (jbossLogger.isEnabled(jbossLevel)) {
+			Object message = arguments == null || arguments.length == 0 ? obj : formatter.format(String.valueOf(obj), arguments);
+			String loggerClassName = RuntimeProvider.getCallerClassName(depth - 1);
+			jbossLogger.log(jbossLevel, loggerClassName, message, exception);
+		}
+	}
+
+	@Override
+	public void log(final String loggerClassName, final String tag, final Level level, final Throwable exception, final Object obj,
+		final Object... arguments) {
+		String callerClassName = RuntimeProvider.getCallerClassName(loggerClassName);
+		Logger jbossLogger = Logger.getLogger(callerClassName);
+		org.jboss.logging.Logger.Level jbossLevel = translate(level);
+
+		if (jbossLogger.isEnabled(jbossLevel)) {
+			Object message = arguments == null || arguments.length == 0 ? obj : formatter.format(String.valueOf(obj), arguments);
+			jbossLogger.log(jbossLevel, loggerClassName, message, exception);
+		}
+	}
+
+	@Override
+	public void shutdown() {
+		// Should be ignored
+	}
+
+	/**
+	 * Translates a tinylog severity level into a JBoss Logging level.
+	 * 
+	 * @param level
+	 *            Severity level of tinylog
+	 * @return Corresponding level of JBoss Logging
+	 */
+	private static org.jboss.logging.Logger.Level translate(final Level level) {
+		switch (level) {
+			case TRACE:
+				return org.jboss.logging.Logger.Level.TRACE;
+			case DEBUG:
+				return org.jboss.logging.Logger.Level.DEBUG;
+			case INFO:
+				return org.jboss.logging.Logger.Level.INFO;
+			case WARN:
+				return org.jboss.logging.Logger.Level.WARN;
+			case ERROR:
+				return org.jboss.logging.Logger.Level.ERROR;
+			default:
+				return org.jboss.logging.Logger.Level.FATAL;
+		}
+	}
+
+}
---
+++ tinylog-jboss/src/test/java/org/tinylog/adapter/jboss/StorageHandler.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.adapter.jboss;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+/**
+ * Simple handler for {@code java.util.logging} that just stores all {@link LogRecord} in a list.
+ */
+public class StorageHandler extends Handler {
+
+	private final List<LogRecord> records;
+
+	/** */
+	public StorageHandler() {
+		records = new ArrayList<>();
+	}
+
+	/**
+	 * Gets a copy of all received log records.
+	 * 
+	 * @return Received log records
+	 */
+	public List<LogRecord> getRecords() {
+		return new ArrayList<LogRecord>(records);
+	}
+
+	@Override
+	public void publish(final LogRecord record) {
+		record.getSourceClassName(); // Ensure that caller is resolved properly
+		records.add(record);
+	}
+
+	@Override
+	public void flush() {
+		// Ignore
+	}
+
+	@Override
+	public void close() {
+		records.clear();
+	}
+
+}
