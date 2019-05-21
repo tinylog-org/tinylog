640b35b2be12982fd2b73faabe34229c66c74c7f
Martin Winandy <martin.winandy@pmwmedia.de>
2019-05-21 21:21:13 +0200

    Add logging provider for forwarding log entries to java.util.logging

---
+++ tinylog-jul/src/main/java/org/tinylog/adapter/jul/JavaUtilLoggingProvider.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2019 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.adapter.jul;
+
+import java.util.Locale;
+import java.util.logging.Logger;
+
+import org.tinylog.Level;
+import org.tinylog.provider.ContextProvider;
+import org.tinylog.provider.LoggingProvider;
+import org.tinylog.provider.MessageFormatter;
+import org.tinylog.provider.NopContextProvider;
+import org.tinylog.runtime.RuntimeProvider;
+
+/**
+ * Logging provider that forwards all log entries to {@code java.util.logging}.
+ */
+public final class JavaUtilLoggingProvider implements LoggingProvider {
+
+	private ContextProvider contextProvider;
+	private MessageFormatter formatter;
+
+	/** */
+	public JavaUtilLoggingProvider() {
+		contextProvider = new NopContextProvider();
+		formatter = new MessageFormatter(Locale.getDefault());
+	}
+
+	@Override
+	public ContextProvider getContextProvider() {
+		return contextProvider;
+	}
+
+	@Override
+	public Level getMinimumLevel() {
+		return Level.TRACE;
+	}
+
+	@Override
+	public Level getMinimumLevel(final String tag) {
+		return Level.TRACE;
+	}
+
+	@Override
+	public boolean isEnabled(final int depth, final String tag, final Level level) {
+		String callerClassName = RuntimeProvider.getCallerClassName(depth + 1);
+		return Logger.getLogger(callerClassName).isLoggable(translate(level));
+	}
+
+	@Override
+	public void log(final int depth, final String tag, final Level level, final Throwable exception, final Object obj,
+		final Object... arguments) {
+		StackTraceElement caller = RuntimeProvider.getCallerStackTraceElement(depth + 1);
+		Logger julLogger = Logger.getLogger(caller.getClassName());
+		java.util.logging.Level julLevel = translate(level);
+		
+		if (julLogger.isLoggable(julLevel)) {
+			String message = String.valueOf(obj);
+			if (arguments != null && arguments.length > 0) {
+				message = formatter.format(message, arguments);
+			}
+			julLogger.logp(julLevel, caller.getClassName(), caller.getMethodName(), message, exception);
+		}
+	}
+
+	@Override
+	public void log(final String loggerClassName, final String tag, final Level level, final Throwable exception, final Object obj,
+		final Object... arguments) {
+		StackTraceElement caller = RuntimeProvider.getCallerStackTraceElement(loggerClassName);
+		Logger julLogger = Logger.getLogger(caller.getClassName());
+		java.util.logging.Level julLevel = translate(level);
+		
+		if (julLogger.isLoggable(julLevel)) {
+			String message = String.valueOf(obj);
+			if (arguments != null && arguments.length > 0) {
+				message = formatter.format(message, arguments);
+			}
+			julLogger.logp(julLevel, caller.getClassName(), caller.getMethodName(), message, exception);
+		}
+	}
+
+	@Override
+	public void shutdown() {
+		// Should be ignored
+	}
+
+	/**
+	 * Translates a tinylog severity level into a {@code java.util.logging} level.
+	 * 
+	 * @param level
+	 *            Severity level of tinylog
+	 * @return Corresponding level of {@code java.util.logging}
+	 */
+	private static java.util.logging.Level translate(final Level level) {
+		switch (level) {
+			case TRACE:
+				return java.util.logging.Level.FINER;
+			case DEBUG:
+				return java.util.logging.Level.FINE;
+			case INFO:
+				return java.util.logging.Level.INFO;
+			case WARN:
+				return java.util.logging.Level.WARNING;
+			case ERROR:
+				return java.util.logging.Level.SEVERE;
+			default:
+				return java.util.logging.Level.OFF;
+		}
+	}
+
+}
---
+++ tinylog-jul/src/test/java/org/tinylog/adapter/jul/JavaUtilLoggingProviderTest.java
@@ -0,0 +1,217 @@
+/*
+ * Copyright 2019 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.adapter.jul;
+
+import java.util.logging.LogManager;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.tinylog.Level;
+import org.tinylog.provider.ContextProvider;
+import org.tinylog.provider.NopContextProvider;
+import org.tinylog.util.StorageHandler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link JavaUtilLoggingProvider}.
+ */
+public final class JavaUtilLoggingProviderTest {
+
+	private StorageHandler handler;
+	private Logger logger;
+
+	/**
+	 * Initializes loggers and storage handler.
+	 */
+	@Before
+	public void initHandler() {
+		handler = new StorageHandler();
+
+		LogManager.getLogManager().reset();
+
+		logger = Logger.getLogger("");
+		logger.addHandler(handler);
+		logger.setLevel(java.util.logging.Level.INFO);
+	}
+
+	/**
+	 * Releases storage handler and resets log manager.
+	 */
+	@After
+	public void releaseHandler() {
+		handler.close();
+		LogManager.getLogManager().reset();
+	}
+
+	/**
+	 * Verifies that {@link NopContextProvider} is used as context provider.
+	 */
+	@Test
+	public void contextProvider() {
+		ContextProvider contextProvider = new JavaUtilLoggingProvider().getContextProvider();
+		assertThat(contextProvider).isInstanceOf(NopContextProvider.class);
+	}
+
+	/**
+	 * Verifies that {@link Level#TRACE} is used as global minimum level.
+	 */
+	@Test
+	public void globalMinimumLevel() {
+		JavaUtilLoggingProvider provider = new JavaUtilLoggingProvider();
+		assertThat(provider.getMinimumLevel()).isEqualTo(Level.TRACE);
+	}
+
+	/**
+	 * Verifies that {@link Level#TRACE} is used as minimum level for all tags.
+	 */
+	@Test
+	public void taggedMinimumLevel() {
+		JavaUtilLoggingProvider provider = new JavaUtilLoggingProvider();
+
+		assertThat(provider.getMinimumLevel(null)).isEqualTo(Level.TRACE);
+		assertThat(provider.getMinimumLevel("MyTag")).isEqualTo(Level.TRACE);
+	}
+
+	/**
+	 * Verifies that {@link Level#INFO}, {@link Level#WARN}, and {@link Level#ERROR} are enabled.
+	 */
+	@Test
+	public void enabled() {
+		JavaUtilLoggingProvider provider = new JavaUtilLoggingProvider();
+
+		assertThat(provider.isEnabled(1, null, Level.INFO)).isTrue();
+		assertThat(provider.isEnabled(1, null, Level.WARN)).isTrue();
+		assertThat(provider.isEnabled(1, null, Level.ERROR)).isTrue();
+	}
+
+	/**
+	 * Verifies that {@link Level#TRACE} and {@link Level#DEBUG} are disabled.
+	 */
+	@Test
+	public void disabled() {
+		JavaUtilLoggingProvider provider = new JavaUtilLoggingProvider();
+
+		assertThat(provider.isEnabled(1, null, Level.TRACE)).isFalse();
+		assertThat(provider.isEnabled(1, null, Level.DEBUG)).isFalse();
+	}
+
+	/**
+	 * Verifies that a plain text message will be discarded if logged at debug level.
+	 */
+	@Test
+	public void logPlainTextMessageAtDebugWithDepthOfCaller() {
+		JavaUtilLoggingProvider provider = new JavaUtilLoggingProvider();
+
+		Exception exception = new RuntimeException();	
+		provider.log(JavaUtilLoggingProvider.class.getName(), null, Level.DEBUG, exception, "Hello World", new Object[0]);
+		assertThat(handler.getRecords()).isEmpty();
+	}
+
+	/**
+	 * Verifies that a plain text message will be output if logged at info level.
+	 */
+	@Test
+	public void logPlainTextMessageAtInfoWithDepthOfCaller() {
+		JavaUtilLoggingProvider provider = new JavaUtilLoggingProvider();
+
+		Exception exception = new RuntimeException();	
+		provider.log(1, null, Level.INFO, exception, "Hello World", new Object[0]);
+		assertThat(handler.getRecords()).hasSize(1);
+
+		LogRecord record = handler.getRecords().get(0);
+		assertThat(record.getLevel()).isEqualTo(java.util.logging.Level.INFO);
+		assertThat(record.getThrown()).isEqualTo(exception);
+		assertThat(record.getMessage()).isEqualTo("Hello World");
+		assertThat(record.getSourceClassName()).isEqualTo(JavaUtilLoggingProviderTest.class.getName());
+	}
+
+	/**
+	 * Verifies that a parameterized message will be output if logged at info level.
+	 */
+	@Test
+	public void logParameterizedMessageAtInfoWithDepthOfCaller() {
+		JavaUtilLoggingProvider provider = new JavaUtilLoggingProvider();
+
+		Exception exception = new RuntimeException();
+		provider.log(1, null, Level.INFO, exception, "Hello {}", new Object[] { "User" });
+		assertThat(handler.getRecords()).hasSize(1);
+
+		LogRecord record = handler.getRecords().get(0);
+		assertThat(record.getLevel()).isEqualTo(java.util.logging.Level.INFO);
+		assertThat(record.getThrown()).isEqualTo(exception);
+		assertThat(record.getMessage()).isEqualTo("Hello User");
+		assertThat(record.getSourceClassName()).isEqualTo(JavaUtilLoggingProviderTest.class.getName());
+	}
+
+	/**
+	 * Verifies that a plain text message will be discarded if logged at debug level.
+	 */
+	@Test
+	public void logPlainTextMessageAtDebugWithLoggerClassName() {
+		JavaUtilLoggingProvider provider = new JavaUtilLoggingProvider();
+
+		Exception exception = new RuntimeException();
+		provider.log(1, null, Level.DEBUG, exception, "Hello World", new Object[0]);
+		assertThat(handler.getRecords()).isEmpty();
+	}
+
+	/**
+	 * Verifies that a plain text message will be output if logged at info level.
+	 */
+	@Test
+	public void logPlainTextMessageAtInfoWithLoggerClassName() {
+		JavaUtilLoggingProvider provider = new JavaUtilLoggingProvider();
+
+		Exception exception = new RuntimeException();
+		provider.log(JavaUtilLoggingProvider.class.getName(), null, Level.INFO, exception, "Hello World", new Object[0]);
+		assertThat(handler.getRecords()).hasSize(1);
+
+		LogRecord record = handler.getRecords().get(0);
+		assertThat(record.getLevel()).isEqualTo(java.util.logging.Level.INFO);
+		assertThat(record.getThrown()).isEqualTo(exception);
+		assertThat(record.getMessage()).isEqualTo("Hello World");
+		assertThat(record.getSourceClassName()).isEqualTo(JavaUtilLoggingProviderTest.class.getName());
+	}
+
+	/**
+	 * Verifies that a parameterized message will be output if logged at info level.
+	 */
+	@Test
+	public void logParameterizedMessageAtInfoWithLoggerClassName() {
+		JavaUtilLoggingProvider provider = new JavaUtilLoggingProvider();
+
+		Exception exception = new RuntimeException();
+		provider.log(JavaUtilLoggingProvider.class.getName(), null, Level.INFO, exception, "Hello {}", new Object[] { "User" });
+		assertThat(handler.getRecords()).hasSize(1);
+
+		LogRecord record = handler.getRecords().get(0);
+		assertThat(record.getLevel()).isEqualTo(java.util.logging.Level.INFO);
+		assertThat(record.getThrown()).isEqualTo(exception);
+		assertThat(record.getMessage()).isEqualTo("Hello User");
+		assertThat(record.getSourceClassName()).isEqualTo(JavaUtilLoggingProviderTest.class.getName());
+	}
+
+	/**
+	 * Verifies that logging provider can be shutdowned without throwing any exception.
+	 */
+	@Test
+	public void shutdown() {
+		new JavaUtilLoggingProvider().shutdown();
+	}
+
+}
