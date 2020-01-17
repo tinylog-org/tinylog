a6bc96c07037fe72b01efafe4918a054a61b08f5
Martin Winandy <martin.winandy@pmwmedia.de>
2020-01-17 16:37:51 +0100

    Add legacy message formatter for third-party logging API like SLF4J
    See #132

---
+++ tinylog-api/src/main/java/org/tinylog/format/AbstractMessageFormatter.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2020 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.format;
+
+import org.tinylog.Supplier;
+
+/**
+ * Abstract base class for message formatter implementations.
+ */
+public abstract class AbstractMessageFormatter implements MessageFormatter {
+
+	/** */
+	protected AbstractMessageFormatter() {
+	}
+
+	/**
+	 * Resolves a potential lazy argument.
+	 * 
+	 * @param argument
+	 *            {@link Supplier} or any other object
+	 * @return Generated value, if passed argument is a {@link Supplier}, otherwise the original passed object
+	 */
+	protected static Object resolve(final Object argument) {
+		if (argument instanceof Supplier<?>) {
+			return ((Supplier<?>) argument).get();
+		} else {
+			return argument;
+		}
+	}
+
+}
---
+++ tinylog-api/src/main/java/org/tinylog/format/LegacyMessageFormatter.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 Martin Winandy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.tinylog.format;
+
+import java.text.MessageFormat;
+
+/**
+ * Message formatter that replaces '{}' placeholders with given arguments.
+ *
+ * <p>
+ * Unlike {@link MessageFormat}, there are no argument indices in placeholders. Instead, the order of arguments counts.
+ * </p>
+ */
+public class LegacyMessageFormatter extends AbstractMessageFormatter {
+
+	private static final int ADDITIONAL_STRING_BUILDER_CAPACITY = 32;
+
+	/** */
+	public LegacyMessageFormatter() {
+	}
+
+	@Override
+	public String format(final String message, final Object[] arguments) {
+		int length = message.length();
+		StringBuilder builder = new StringBuilder(length + ADDITIONAL_STRING_BUILDER_CAPACITY);
+		int argumentIndex = 0;
+
+		for (int index = 0; index < length; ++index) {
+			char character = message.charAt(index);
+			if (character == '{' && index + 1 < length && message.charAt(index + 1) == '}' && argumentIndex < arguments.length) {
+				builder.append(resolve(arguments[argumentIndex++]));
+				index += 1; // Skip over closing curly bracket
+			} else {
+				builder.append(character);
+			}
+		}
+
+		return builder.toString();
+	}
+
+}
