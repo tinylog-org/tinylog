11e9796a0d0cab8d94431b72175e6300488c040f
Martin Winandy <martin.winandy@pmwmedia.de>
2020-08-20 10:32:57 +0200

    Remove unused imports

--- tinylog-impl/src/test/java/org/tinylog/writers/RollingFileWriterTest.java
+++ tinylog-impl/src/test/java/org/tinylog/writers/RollingFileWriterTest.java
@@ -38,8 +38,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assume.assumeTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
