2fa7d3399d820d201c7a08d1d1ac79cffdc46c66
Martin Winandy <martin.winandy@tinylog.org>
2020-08-21 23:28:27 +0200

    Add Gradle test project for Android

--- tinylog-core-base/src/test/java/org/tinylog/core/format/value/JavaTimeFormatTest.java
+++ tinylog-core-base/src/test/java/org/tinylog/core/format/value/JavaTimeFormatTest.java
@@ -17,7 +17,6 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.Locale;
