9d937da1f03dda7445314f4c04a7b0f1a2c6a3ae
Martin Winandy <martin.winandy@pmwmedia.de>
2020-11-06 22:16:17 +0100

    Provide license as separate file instead of source file header
    According to the Apache License v2, the source file header is recommended but not required.

--- tinylog-core-base/src/test/java/org/tinylog/core/runtime/JavaClassNameBasedStackTraceLocationTest.java
+++ tinylog-core-base/src/test/java/org/tinylog/core/runtime/JavaClassNameBasedStackTraceLocationTest.java
@@ -1,16 +1,3 @@
-/*
- * Copyright 2020 Martin Winandy
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
-
 package org.tinylog.core.runtime;
 
 import java.util.function.Supplier;
@@ -61,7 +48,7 @@ class JavaClassNameBasedStackTraceLocationTest {
 			.isEqualTo(JavaClassNameBasedStackTraceLocationTest.class.getSimpleName() + ".java");
 		assertThat(caller.getClassName()).isEqualTo(JavaClassNameBasedStackTraceLocationTest.class.getName());
 		assertThat(caller.getMethodName()).isEqualTo("validCallerStackTraceElement");
-		assertThat(caller.getLineNumber()).isEqualTo(57);
+		assertThat(caller.getLineNumber()).isEqualTo(44);
 	}
 
 	/**
@@ -90,7 +77,7 @@ class JavaClassNameBasedStackTraceLocationTest {
 			.isEqualTo(JavaClassNameBasedStackTraceLocationTest.class.getSimpleName() + ".java");
 		assertThat(caller.getClassName()).isEqualTo(JavaClassNameBasedStackTraceLocationTest.class.getName());
 		assertThat(caller.getMethodName()).isEqualTo("passedStackTraceLocation");
-		assertThat(caller.getLineNumber()).isEqualTo(86);
+		assertThat(caller.getLineNumber()).isEqualTo(73);
 	}
 
 	/**
--- tinylog-core-modern/src/test/java/org/tinylog/core/runtime/JavaIndexBasedStackTraceLocationTest.java
+++ tinylog-core-modern/src/test/java/org/tinylog/core/runtime/JavaIndexBasedStackTraceLocationTest.java
@@ -1,16 +1,3 @@
-/*
- * Copyright 2020 Martin Winandy
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
-
 package org.tinylog.core.runtime;
 
 import javax.inject.Inject;
@@ -73,7 +60,7 @@ class JavaIndexBasedStackTraceLocationTest {
 			.isEqualTo(JavaIndexBasedStackTraceLocationTest.class.getSimpleName() + ".java");
 		assertThat(caller.getClassName()).isEqualTo(JavaIndexBasedStackTraceLocationTest.class.getName());
 		assertThat(caller.getMethodName()).isEqualTo("validCallerStackTraceElement");
-		assertThat(caller.getLineNumber()).isEqualTo(69);
+		assertThat(caller.getLineNumber()).isEqualTo(56);
 	}
 
 	/**
@@ -110,7 +97,7 @@ class JavaIndexBasedStackTraceLocationTest {
 			.isEqualTo(JavaIndexBasedStackTraceLocationTest.class.getSimpleName() + ".java");
 		assertThat(caller.getClassName()).isEqualTo(JavaIndexBasedStackTraceLocationTest.class.getName());
 		assertThat(caller.getMethodName()).isEqualTo("passedStackTraceLocation");
-		assertThat(caller.getLineNumber()).isEqualTo(106);
+		assertThat(caller.getLineNumber()).isEqualTo(93);
 	}
 
 	/**
