32d621e76b6fa95269c8825746160fd97c265ed7
Martin Winandy <martin.winandy@pmwmedia.de>
2020-11-14 23:43:11 +0100

    Add getters for checking if severity levels are enabled to tagged logger

--- tinylog-api/src/main/java/org/tinylog/TaggedLogger.java
+++ tinylog-api/src/main/java/org/tinylog/TaggedLogger.java
@@ -1,6 +1,7 @@
 package org.tinylog;
 
 import org.tinylog.core.Framework;
+import org.tinylog.core.Level;
 import org.tinylog.core.Tinylog;
 import org.tinylog.core.backend.LevelVisibility;
 import org.tinylog.core.backend.LoggingBackend;
@@ -10,6 +11,8 @@ import org.tinylog.core.runtime.RuntimeFlavor;
  * Logger for issuing tagged log entries.
  */
 public final class TaggedLogger {
+
+	private static final int CALLER_STACK_TRACE_DEPTH = 2;
 
 	private final String tag;
 	private final RuntimeFlavor runtime;
@@ -41,6 +44,81 @@ public final class TaggedLogger {
 	 */
 	public String getTag() {
 		return tag;
+	}
+
+	/**
+	 * Checks if the trace severity level is enabled for the actual class and tag.
+	 *
+	 * <p>
+	 *     If this method returns {@code true}, an issued trace log entry will be output. If this method returns
+	 *     {@code false}, issued trace log entries will be discarded. 
+	 * </p>
+	 *
+	 * @return {@code true} if enabled, otherwise {@code false}
+	 */
+	public boolean isTraceEnabled() {
+		return visibility.isTraceEnabled()
+			&& backend.isEnabled(runtime.getStackTraceLocationAtIndex(CALLER_STACK_TRACE_DEPTH), tag, Level.TRACE);
+	}
+
+	/**
+	 * Checks if the debug severity level is enabled for the actual class and tag.
+	 *
+	 * <p>
+	 *     If this method returns {@code true}, an issued debug log entry will be output. If this method returns
+	 *     {@code false}, issued debug log entries will be discarded.
+	 * </p>
+	 *
+	 * @return {@code true} if enabled, otherwise {@code false}
+	 */
+	public boolean isDebugEnabled() {
+		return visibility.isDebugEnabled()
+			&& backend.isEnabled(runtime.getStackTraceLocationAtIndex(CALLER_STACK_TRACE_DEPTH), tag, Level.DEBUG);
+	}
+
+	/**
+	 * Checks if the info severity level is enabled for the actual class and tag.
+	 *
+	 * <p>
+	 *     If this method returns {@code true}, an issued info log entry will be output. If this method returns
+	 *     {@code false}, issued info log entries will be discarded.
+	 * </p>
+	 *
+	 * @return {@code true} if enabled, otherwise {@code false}
+	 */
+	public boolean isInfoEnabled() {
+		return visibility.isInfoEnabled()
+			&& backend.isEnabled(runtime.getStackTraceLocationAtIndex(CALLER_STACK_TRACE_DEPTH), tag, Level.INFO);
+	}
+
+	/**
+	 * Checks if the warn severity level is enabled for the actual class and tag.
+	 *
+	 * <p>
+	 *     If this method returns {@code true}, an issued warn log entry will be output. If this method returns
+	 *     {@code false}, issued warn log entries will be discarded.
+	 * </p>
+	 *
+	 * @return {@code true} if enabled, otherwise {@code false}
+	 */
+	public boolean isWarnEnabled() {
+		return visibility.isWarnEnabled()
+			&& backend.isEnabled(runtime.getStackTraceLocationAtIndex(CALLER_STACK_TRACE_DEPTH), tag, Level.WARN);
+	}
+
+	/**
+	 * Checks if the error severity level is enabled for the actual class and tag.
+	 *
+	 * <p>
+	 *     If this method returns {@code true}, an issued error log entry will be output. If this method returns
+	 *     {@code false}, issued error log entries will be discarded.
+	 * </p>
+	 *
+	 * @return {@code true} if enabled, otherwise {@code false}
+	 */
+	public boolean isErrorEnabled() {
+		return visibility.isErrorEnabled()
+			&& backend.isEnabled(runtime.getStackTraceLocationAtIndex(CALLER_STACK_TRACE_DEPTH), tag, Level.ERROR);
 	}
 
 }
--- tinylog-api/src/test/java/org/tinylog/TaggedLoggerTest.java
+++ tinylog-api/src/test/java/org/tinylog/TaggedLoggerTest.java
@@ -1,18 +1,37 @@
 package org.tinylog;
 
-import javax.inject.Inject;
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.tinylog.core.Framework;
-import org.tinylog.core.test.log.CaptureLogEntries;
+import org.tinylog.core.Level;
+import org.tinylog.core.backend.LevelVisibility;
+import org.tinylog.core.backend.LoggingBackend;
+import org.tinylog.core.runtime.StackTraceLocation;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.notNull;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 
-@CaptureLogEntries
+@ExtendWith(MockitoExtension.class)
 class TaggedLoggerTest {
 
-	@Inject
-	private Framework framework;
+	@Mock
+	private LoggingBackend backend;
+
+	private final Framework framework = new Framework(false, false) {
+
+		@Override
+		public LoggingBackend getLoggingBackend() {
+			return backend;
+		}
+
+	};
 
 	/**
 	 * Verifies that a string can be assigned as tag.
@@ -30,6 +49,101 @@ class TaggedLoggerTest {
 	void nullTag() {
 		TaggedLogger logger = new TaggedLogger(null, framework);
 		assertThat(logger.getTag()).isNull();
+	}
+
+	/**
+	 * Verifies the results of the {@link TaggedLogger#isTraceEnabled()} method.
+	 *
+	 * @param visible The value for {@link LevelVisibility#isTraceEnabled()}
+	 * @param enabled The value for {@link LoggingBackend#isEnabled(StackTraceLocation, String, Level)}
+	 */
+	@ParameterizedTest
+	@CsvSource(value = {"false,false", "false,true", "true,false", "true,true"})
+	void isTraceEnabled(boolean visible, boolean enabled) {
+		when(backend.getLevelVisibility("test")).thenReturn(
+			new LevelVisibility(visible, false, false, false, false)
+		);
+
+		lenient().when(backend.isEnabled(notNull(), eq("test"), eq(Level.TRACE))).thenReturn(enabled);
+
+		TaggedLogger logger = new TaggedLogger("test", framework);
+		assertThat(logger.isTraceEnabled()).isEqualTo(visible && enabled);
+	}
+
+	/**
+	 * Verifies the results of the {@link TaggedLogger#isDebugEnabled()} method.
+	 *
+	 * @param visible The value for {@link LevelVisibility#isDebugEnabled()}
+	 * @param enabled The value for {@link LoggingBackend#isEnabled(StackTraceLocation, String, Level)}
+	 */
+	@ParameterizedTest
+	@CsvSource(value = {"false,false", "false,true", "true,false", "true,true"})
+	void isDebugEnabled(boolean visible, boolean enabled) {
+		when(backend.getLevelVisibility("test")).thenReturn(
+			new LevelVisibility(true, visible, false, false, false)
+		);
+
+		lenient().when(backend.isEnabled(notNull(), eq("test"), eq(Level.DEBUG))).thenReturn(enabled);
+
+		TaggedLogger logger = new TaggedLogger("test", framework);
+		assertThat(logger.isDebugEnabled()).isEqualTo(visible && enabled);
+	}
+
+	/**
+	 * Verifies the results of the {@link TaggedLogger#isInfoEnabled()} method.
+	 *
+	 * @param visible The value for {@link LevelVisibility#isInfoEnabled()}
+	 * @param enabled The value for {@link LoggingBackend#isEnabled(StackTraceLocation, String, Level)}
+	 */
+	@ParameterizedTest
+	@CsvSource(value = {"false,false", "false,true", "true,false", "true,true"})
+	void isInfoEnabled(boolean visible, boolean enabled) {
+		when(backend.getLevelVisibility("test")).thenReturn(
+			new LevelVisibility(true, true, visible, false, false)
+		);
+
+		lenient().when(backend.isEnabled(notNull(), eq("test"), eq(Level.INFO))).thenReturn(enabled);
+
+		TaggedLogger logger = new TaggedLogger("test", framework);
+		assertThat(logger.isInfoEnabled()).isEqualTo(visible && enabled);
+	}
+
+	/**
+	 * Verifies the results of the {@link TaggedLogger#isWarnEnabled()} method.
+	 *
+	 * @param visible The value for {@link LevelVisibility#isWarnEnabled()}
+	 * @param enabled The value for {@link LoggingBackend#isEnabled(StackTraceLocation, String, Level)}
+	 */
+	@ParameterizedTest
+	@CsvSource(value = {"false,false", "false,true", "true,false", "true,true"})
+	void isWarnEnabled(boolean visible, boolean enabled) {
+		when(backend.getLevelVisibility("test")).thenReturn(
+			new LevelVisibility(true, true, true, visible, false)
+		);
+
+		lenient().when(backend.isEnabled(notNull(), eq("test"), eq(Level.WARN))).thenReturn(enabled);
+
+		TaggedLogger logger = new TaggedLogger("test", framework);
+		assertThat(logger.isWarnEnabled()).isEqualTo(visible && enabled);
+	}
+
+	/**
+	 * Verifies the results of the {@link TaggedLogger#isErrorEnabled()} method.
+	 *
+	 * @param visible The value for {@link LevelVisibility#isErrorEnabled()}
+	 * @param enabled The value for {@link LoggingBackend#isEnabled(StackTraceLocation, String, Level)}
+	 */
+	@ParameterizedTest
+	@CsvSource(value = {"false,false", "false,true", "true,false", "true,true"})
+	void isErrorEnabled(boolean visible, boolean enabled) {
+		when(backend.getLevelVisibility("test")).thenReturn(
+			new LevelVisibility(true, true, true, true, visible)
+		);
+
+		lenient().when(backend.isEnabled(notNull(), eq("test"), eq(Level.ERROR))).thenReturn(enabled);
+
+		TaggedLogger logger = new TaggedLogger("test", framework);
+		assertThat(logger.isErrorEnabled()).isEqualTo(visible && enabled);
 	}
 
 }
