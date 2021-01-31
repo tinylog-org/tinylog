e84ac4508b8a7524010d1292de134bd8d2e4e1c1
Martin Winandy <martin.winandy@pmwmedia.de>
2021-01-31 11:54:30 +0100

    Add placeholder renderer for JUnit tests

---
+++ tinylog-impl/src/test/java/org/tinylog/impl/test/PlaceholderRenderer.java
@@ -0,0 +1,32 @@
+package org.tinylog.impl.test;
+
+import org.tinylog.impl.LogEntry;
+import org.tinylog.impl.format.Placeholder;
+
+/**
+ * Renderer for {@link Placeholder Placeholders}.
+ */
+public class PlaceholderRenderer {
+
+	private final Placeholder placeholder;
+
+	/**
+	 * @param placeholder The placeholder to render
+	 */
+	public PlaceholderRenderer(Placeholder placeholder) {
+		this.placeholder = placeholder;
+	}
+
+	/**
+	 * Renders the stored placeholder with the passed log entry as input.
+	 * 
+	 * @param logEntry The log entry as input for {@link Placeholder#render(StringBuilder, LogEntry)}
+	 * @return The render result
+	 */
+	public String render(LogEntry logEntry) {
+		StringBuilder builder = new StringBuilder();
+		placeholder.render(builder, logEntry);
+		return builder.toString();
+	}
+
+}
