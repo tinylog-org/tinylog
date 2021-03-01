c7b0ba264e655949d43b625cf616501d23146e54
Martin Winandy <martin.winandy@pmwmedia.de>
2021-03-01 22:15:04 +0100

    Add file writer

---
+++ tinylog-impl/src/main/java/org/tinylog/impl/writer/FileWriter.java
@@ -0,0 +1,131 @@
+package org.tinylog.impl.writer;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Set;
+
+import org.tinylog.impl.LogEntry;
+import org.tinylog.impl.LogEntryValue;
+import org.tinylog.impl.format.placeholder.Placeholder;
+
+/**
+ * Asynchronous writer for writing log entries to a file.
+ */
+public class FileWriter implements AsyncWriter {
+
+	private static final int BUILDER_START_CAPACITY = 1024;
+	private static final int BUILDER_MAX_CAPACITY = 65536;
+	private static final int CHUNK_CAPACITY = 65536;
+
+	private final Placeholder placeholder;
+	private final RandomAccessFile file;
+	private final CharsetEncoder encoder;
+
+	private StringBuilder builder;
+
+	private final byte[] chunk;
+	private int chunkMaxSize;
+	private int chunkSize;
+
+	/**
+	 * @param placeholder The placeholder for formatting log entries
+	 * @param file The path to the target log file
+	 * @param charset The charset to use for writing strings to the target file
+	 * @throws IOException Failed to access the target log file
+	 */
+	public FileWriter(Placeholder placeholder, Path file, Charset charset) throws IOException {
+		Path parent = file.toAbsolutePath().getParent();
+		if (parent != null) {
+			Files.createDirectories(parent);
+		}
+
+		this.placeholder = placeholder;
+		this.file = new RandomAccessFile(file.toString(), "rw");
+		this.encoder = charset.newEncoder();
+
+		this.builder = new StringBuilder(BUILDER_START_CAPACITY);
+
+		this.chunk = new byte[CHUNK_CAPACITY];
+		this.chunkMaxSize = CHUNK_CAPACITY - (int) (this.file.length() % CHUNK_CAPACITY);
+		this.chunkSize = 0;
+	}
+
+	@Override
+	public Set<LogEntryValue> getRequiredLogEntryValues() {
+		return placeholder.getRequiredLogEntryValues();
+	}
+
+	@Override
+	public void log(LogEntry entry) throws IOException {
+		try {
+			placeholder.render(builder, entry);
+			storeStringBuilder();
+		} finally {
+			resetStringBuilder();
+		}
+	}
+
+	@Override
+	public void flush() throws IOException {
+		if (chunkSize > 0) {
+			file.write(chunk, 0, chunkSize);
+			chunkMaxSize = CHUNK_CAPACITY == chunkSize ? CHUNK_CAPACITY : CHUNK_CAPACITY - chunkSize;
+			chunkSize = 0;
+		}
+	}
+
+	@Override
+	public void close() throws IOException {
+		try {
+			flush();
+		} finally {
+			file.close();
+		}
+	}
+
+	/**
+	 * Stores the content of the string builder.
+	 *
+	 * @throws IOException Failed to encode the log entry or to write it to the target file
+	 */
+	private void storeStringBuilder() throws IOException {
+		ByteBuffer buffer = encoder.encode(CharBuffer.wrap(builder));
+		byte[] data = buffer.array();
+		int dataLength = buffer.limit();
+
+		int length = Math.min(dataLength, CHUNK_CAPACITY - chunkSize);
+		System.arraycopy(data, 0, chunk, chunkSize, length);
+		chunkSize += length;
+
+		if (chunkSize >= chunkMaxSize) {
+			flush();
+
+			int index = length;
+			length = ((buffer.limit() - index) / CHUNK_CAPACITY) * CHUNK_CAPACITY;
+			file.write(data, index, length);
+
+			index += length;
+			length = dataLength - index;
+			System.arraycopy(data, index, chunk, 0, length);
+			chunkSize = length;
+		}
+	}
+
+	/**
+	 * Resets the string builder for writing the next log entry.
+	 */
+	private void resetStringBuilder() {
+		if (builder.capacity() > BUILDER_MAX_CAPACITY) {
+			builder = new StringBuilder(BUILDER_START_CAPACITY);
+		} else {
+			builder.setLength(0);
+			builder.ensureCapacity(0);
+		}
+	}
+}
---
+++ tinylog-impl/src/test/java/org/tinylog/impl/writer/FileWriterTest.java
@@ -0,0 +1,154 @@
+package org.tinylog.impl.writer;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.tinylog.impl.LogEntry;
+import org.tinylog.impl.LogEntryValue;
+import org.tinylog.impl.format.placeholder.MessagePlaceholder;
+import org.tinylog.impl.test.LogEntryBuilder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FileWriterTest {
+
+	private Path logFile;
+
+	/**
+	 * Creates a temporary log file.
+	 *
+	 * @throws IOException Failed to create a temporary log file
+	 */
+	@BeforeEach
+	void init() throws IOException {
+		logFile = Files.createTempFile("tinylog", ".log");
+		logFile.toFile().deleteOnExit();
+	}
+
+	/**
+	 * Deletes the created temporary log file.
+	 *
+	 * @throws IOException Failed to delete the temporary log file
+	 */
+	@AfterEach
+	void release() throws IOException {
+		Files.deleteIfExists(logFile);
+	}
+
+	/**
+	 * Verifies that the file writer requires only log entry values from the passed placeholder.
+	 */
+	@Test
+	void requiredLogEntryValues() throws IOException {
+		FileWriter writer = new FileWriter(new MessagePlaceholder(), logFile, StandardCharsets.UTF_8);
+		try {
+			assertThat(writer.getRequiredLogEntryValues())
+				.containsExactlyInAnyOrder(LogEntryValue.MESSAGE, LogEntryValue.EXCEPTION);
+		} finally {
+			writer.close();
+		}
+	}
+
+	/**
+	 * Verifies that a single short string (smaller than the used buffer) is written to the log file.
+	 */
+	@Test
+	void writeSingleShortMessage() throws IOException {
+		FileWriter writer = new FileWriter(new MessagePlaceholder(), logFile, StandardCharsets.UTF_8);
+		try {
+			LogEntry entry = new LogEntryBuilder().message("Hello World!").create();
+			writer.log(entry);
+		} finally {
+			writer.close();
+		}
+
+		assertThat(logFile).hasContent("Hello World!");
+	}
+
+	/**
+	 * Verifies that multiple short strings (smaller than the used buffer) are written to the log file.
+	 */
+	@Test
+	void writeMultipleShortMessage() throws IOException {
+		FileWriter writer = new FileWriter(new MessagePlaceholder(), logFile, StandardCharsets.UTF_8);
+		try {
+			LogEntry entry = new LogEntryBuilder().message("Hello World!").create();
+			writer.log(entry);
+
+			entry = new LogEntryBuilder().message("Goodbye.").create();
+			writer.log(entry);
+		} finally {
+			writer.close();
+		}
+
+		assertThat(logFile).hasContent("Hello World!Goodbye.");
+	}
+
+	/**
+	 * Verifies that a long string (larger than the used buffer) is written to the log file.
+	 */
+	@Test
+	void writeSingleLongMessage() throws IOException {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < 256 * 1024; ++i) {
+			builder.append(i % 10);
+		}
+
+		FileWriter writer = new FileWriter(new MessagePlaceholder(), logFile, StandardCharsets.UTF_8);
+		try {
+			LogEntry entry = new LogEntryBuilder().message(builder.toString()).create();
+			writer.log(entry);
+		} finally {
+			writer.close();
+		}
+
+		assertThat(logFile).hasContent(builder.toString());
+	}
+
+	/**
+	 * Verifies that multiple long string (larger than the used buffer) are written to the log file.
+	 */
+	@Test
+	void writeMultipleLongMessage() throws IOException {
+		String first = "<";
+
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < 128 * 1024; ++i) {
+			builder.append(i % 10);
+		}
+		String second = builder.toString();
+
+		builder = new StringBuilder();
+		for (int i = 0; i < 128 * 1024; ++i) {
+			builder.append(('a' + i) % 26);
+		}
+		String third = builder.toString();
+
+		String fourth = ">";
+
+		FileWriter writer = new FileWriter(new MessagePlaceholder(), logFile, StandardCharsets.UTF_8);
+		try {
+			LogEntry entry = new LogEntryBuilder().message(first).create();
+			writer.log(entry);
+
+			entry = new LogEntryBuilder().message(second).create();
+			writer.log(entry);
+
+			entry = new LogEntryBuilder().message(third).create();
+			writer.log(entry);
+
+			entry = new LogEntryBuilder().message(fourth).create();
+			writer.log(entry);
+		} finally {
+			writer.close();
+		}
+
+		assertThat(logFile).hasContent(first + second + third + fourth);
+	}
+
+}
