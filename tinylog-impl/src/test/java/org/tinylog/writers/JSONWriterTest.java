9aa3c3733074402f3532727d8e5b215bc30da547
proohit <direnc99@gmail.com>
2021-03-19 15:23:18 +0100

    #194 - Added Unit tests for JSONWriter

--- tinylog-impl/src/test/java/org/tinylog/writers/JSONWriterTest.java
+++ tinylog-impl/src/test/java/org/tinylog/writers/JSONWriterTest.java
@@ -14,22 +14,20 @@
 package org.tinylog.writers;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThat;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.tinylog.core.LogEntry;
 import org.tinylog.rules.SystemStreamCollector;
 import org.tinylog.util.FileSystem;
 import org.tinylog.util.LogEntryBuilder;
 
 public final class JSONWriterTest {
-
-    private static final String NEW_LINE = System.lineSeparator();
 
     /**
      * Redirects and collects system output streams.
@@ -43,29 +41,117 @@ public final class JSONWriterTest {
      * @throws IOException Failed writing to file
      */
     @Test
-    public void unbufferedWriting() throws IOException {
-        // String file = FileSystem.createTemporaryFile();
-        File myObj = new File("logfile.json");
-        myObj.createNewFile();
-        String file = myObj.getName();
+    public void unappendingWriting() throws IOException {
+        String file = FileSystem.createTemporaryFile();
 
         Map<String, String> properties = new HashMap<>();
         properties.put("file", file);
         properties.put("buffered", "false");
         properties.put("append", "false");
+
         JSONWriter writer;
         writer = new JSONWriter(properties);
-        writer.write(LogEntryBuilder.prefilled(JSONWriterTest.class).create());
-        writer.write(LogEntryBuilder.prefilled(JSONWriterTest.class).create());
+        LogEntry givenLogEntry = LogEntryBuilder.prefilled(JSONWriterTest.class).create();
+        writer.write(givenLogEntry);
+        writer.write(givenLogEntry);
         writer.close();
+
         writer = new JSONWriter(properties);
-        writer.write(LogEntryBuilder.prefilled(JSONWriterTest.class).create());
-        writer.write(LogEntryBuilder.prefilled(JSONWriterTest.class).create());
+        writer.write(givenLogEntry);
+        writer.write(givenLogEntry);
         writer.close();
 
-        String resultingText1 = FileSystem.readFile(file);
+        LogEntry expectedLogEntry = givenLogEntry;
+        String expectedMessage = String.format("\"message\": \"%s\"", expectedLogEntry.getMessage());
+        String expectedLevel = String.format("\"level\": \"%s\"", expectedLogEntry.getLevel());
+        String resultingEntry = FileSystem.readFile(file);
 
-        assertThat(resultingText1).contains("1985").contains("03").contains("TRACE").contains("Hello World!");
+        int resultingMessagCount = resultingEntry.split(Pattern.quote(expectedMessage)).length - 1;
+        int resultingLevelCount = resultingEntry.split(Pattern.quote(expectedLevel)).length - 1;
+        assertThat(resultingMessagCount).isEqualTo(2);
+        assertThat(resultingLevelCount).isEqualTo(2);
     }
 
+    @Test
+    public void appendingWriting() throws IOException {
+        String file = FileSystem.createTemporaryFile();
+
+        Map<String, String> properties = new HashMap<>();
+        properties.put("file", file);
+        properties.put("buffered", "false");
+        properties.put("append", "true");
+
+        JSONWriter writer;
+        writer = new JSONWriter(properties);
+        LogEntry givenLogEntry = LogEntryBuilder.prefilled(JSONWriterTest.class).create();
+        writer.write(givenLogEntry);
+        writer.write(givenLogEntry);
+        writer.close();
+
+        writer = new JSONWriter(properties);
+        writer.write(givenLogEntry);
+        writer.write(givenLogEntry);
+        writer.close();
+
+        LogEntry expectedLogEntry = givenLogEntry;
+        String expectedMessage = String.format("\"message\": \"%s\"", expectedLogEntry.getMessage());
+        String expectedLevel = String.format("\"level\": \"%s\"", expectedLogEntry.getLevel());
+        String resultingEntry = FileSystem.readFile(file);
+
+        int resultingMessagCount = resultingEntry.split(Pattern.quote(expectedMessage)).length - 1;
+        int resultingLevelCount = resultingEntry.split(Pattern.quote(expectedLevel)).length - 1;
+        assertThat(resultingMessagCount).isEqualTo(4);
+        assertThat(resultingLevelCount).isEqualTo(4);
+    }
+
+    @Test
+    public void writesArrayCorrectly() throws IOException {
+        String file = FileSystem.createTemporaryFile();
+
+        Map<String, String> properties = new HashMap<>();
+        properties.put("file", file);
+        properties.put("buffered", "false");
+        properties.put("append", "false");
+
+        JSONWriter writer;
+        writer = new JSONWriter(properties);
+        LogEntry givenLogEntry = LogEntryBuilder.prefilled(JSONWriterTest.class).create();
+        writer.write(givenLogEntry);
+        writer.close();
+
+        String resultingEntry = FileSystem.readFile(file);
+
+        int indexOfOpeningArray = resultingEntry.indexOf("[");
+        int indexOfOpeningJsonObject = resultingEntry.indexOf("{");
+        int indexOfClosingJsonObject = resultingEntry.indexOf("}");
+        int indexOfClosingArray = resultingEntry.indexOf("]");
+
+        assertThat(indexOfOpeningJsonObject).isGreaterThan(indexOfOpeningArray).isLessThan(indexOfClosingJsonObject);
+        assertThat(indexOfClosingJsonObject).isLessThan(indexOfClosingArray);
+    }
+
+    @Test
+    public void addsJsonObjectCorrectly() throws IOException {
+        String file = FileSystem.createTemporaryFile();
+
+        Map<String, String> properties = new HashMap<>();
+        properties.put("file", file);
+        properties.put("buffered", "false");
+        properties.put("append", "true");
+
+        JSONWriter writer;
+        writer = new JSONWriter(properties);
+        LogEntry givenLogEntry = LogEntryBuilder.prefilled(JSONWriterTest.class).create();
+        writer.write(givenLogEntry);
+        writer.write(givenLogEntry);
+        writer.close();
+
+        String resultingEntry = FileSystem.readFile(file);
+
+        int indexOfClosingFirstJsonObject = resultingEntry.indexOf("}");
+        int indexOfOpeningSecondJsonObject = resultingEntry.indexOf("{", indexOfClosingFirstJsonObject);
+        int indexOfComma = resultingEntry.indexOf(",", indexOfClosingFirstJsonObject);
+        assertThat(indexOfComma).isLessThan(indexOfOpeningSecondJsonObject)
+                .isGreaterThan(indexOfClosingFirstJsonObject);
+    }
 }
