9d53c4b435eb3e13fc3c900a23d178ff287bd5ad
Martin Winandy <martin.winandy@pmwmedia.de>
2021-04-13 21:55:29 +0200

    Fix date in copyright header

--- tinylog-api/src/test/java/org/tinylog/format/EndlessIteratorTest.java
+++ tinylog-api/src/test/java/org/tinylog/format/EndlessIteratorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright $year Martin Winandy
+ * Copyright 2021 Martin Winandy
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
