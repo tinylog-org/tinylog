0fea565564a07779b7c0b10eecb941c8b4c5ed08
Martin Winandy <martin.winandy@pmwmedia.de>
2021-08-26 23:43:20 +0200

    Add writer repository for sync and async writers

---
+++ tinylog-impl/src/main/java/org/tinylog/impl/backend/WriterRepository.java
@@ -0,0 +1,65 @@
+package org.tinylog.impl.backend;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.tinylog.impl.writer.AsyncWriter;
+import org.tinylog.impl.writer.Writer;
+
+/**
+ * Repository for sync and async writers.
+ */
+public class WriterRepository {
+
+	private final List<Writer> syncWriters;
+	private final List<AsyncWriter> asyncWriters;
+	private final List<Writer> allWriters;
+
+	/**
+	 * @param writers The writers to store
+	 */
+	public WriterRepository(Collection<Writer> writers) {
+		allWriters = new ArrayList<>();
+		syncWriters = new ArrayList<>();
+		asyncWriters = new ArrayList<>();
+
+		for (Writer writer : writers) {
+			allWriters.add(writer);
+
+			if (writer instanceof AsyncWriter) {
+				asyncWriters.add((AsyncWriter) writer);
+			} else {
+				syncWriters.add(writer);
+			}
+		}
+	}
+
+	/**
+	 * Gets all writers.
+	 *
+	 * @return All stored writers
+	 */
+	public Collection<Writer> getAllWriters() {
+		return allWriters;
+	}
+
+	/**
+	 * Gets all sync writers.
+	 *
+	 * @return All stored writes that do not implement the {@link AsyncWriter} interface
+	 */
+	public Collection<Writer> getSyncWriters() {
+		return syncWriters;
+	}
+
+	/**
+	 * Gets all async writers.
+	 *
+	 * @return All stored writes that implement the {@link AsyncWriter} interface
+	 */
+	public Collection<AsyncWriter> getAsyncWriters() {
+		return asyncWriters;
+	}
+
+}
---
+++ tinylog-impl/src/test/java/org/tinylog/impl/backend/WriterRepositoryTest.java
@@ -0,0 +1,29 @@
+package org.tinylog.impl.backend;
+
+import org.junit.jupiter.api.Test;
+import org.tinylog.impl.writer.AsyncWriter;
+import org.tinylog.impl.writer.Writer;
+
+import com.google.common.collect.ImmutableList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+class WriterRepositoryTest {
+
+	/**
+	 * Verifies that sync and async writers are partitioning correctly.
+	 */
+	@Test
+	void partitioningOfWriters() {
+		Writer first = mock(Writer.class);
+		AsyncWriter second = mock(AsyncWriter.class);
+		Writer third = mock(Writer.class);
+
+		WriterRepository repository = new WriterRepository(ImmutableList.of(first, second, third));
+		assertThat(repository.getAllWriters()).containsExactlyInAnyOrder(first, second, third);
+		assertThat(repository.getSyncWriters()).containsExactlyInAnyOrder(first, third);
+		assertThat(repository.getAsyncWriters()).containsExactlyInAnyOrder(second);
+	}
+
+}
