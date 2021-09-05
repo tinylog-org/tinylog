5882fef328779eca48ae6b158043b878da89ad6c
Martin Winandy <martin.winandy@pmwmedia.de>
2021-09-05 23:56:30 +0200

    Add builder for creating native logging back-end

---
+++ tinylog-impl/src/main/java/org/tinylog/impl/backend/NativeLoggingBackendBuilder.java
@@ -0,0 +1,52 @@
+package org.tinylog.impl.backend;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.tinylog.core.Framework;
+import org.tinylog.core.backend.LoggingBackend;
+import org.tinylog.core.backend.LoggingBackendBuilder;
+import org.tinylog.impl.WritingThread;
+import org.tinylog.impl.writers.AsyncWriter;
+import org.tinylog.impl.writers.Writer;
+
+/**
+ * Builder for creating an instance of {@link NativeLoggingBackend}.
+ */
+public class NativeLoggingBackendBuilder implements LoggingBackendBuilder {
+
+	private static final int QUEUE_SIZE = 64 * 1024;
+
+	/** */
+	public NativeLoggingBackendBuilder() {
+	}
+
+	@Override
+	public String getName() {
+		return "tinylog";
+	}
+
+	@Override
+	public LoggingBackend create(Framework framework) {
+		LoggingConfiguration configuration = new LoggingConfigurationParser(framework).parse();
+		WritingThread writingThread = createWritingThread(configuration.getAllWriters());
+		return new NativeLoggingBackend(framework, configuration, writingThread);
+	}
+
+	/**
+	 * Creates a {@link WritingThread} instance if there is at least one {@link AsyncWriter}.
+	 *
+	 * @param writers All sync and async writers
+	 * @return A started instance of {@link WritingThread} if there is at least one {@link AsyncWriter} in the passed
+	 *         writer collection, otherwise {@code null}
+	 */
+	private static WritingThread createWritingThread(Collection<Writer> writers) {
+		Collection<AsyncWriter> asyncWriters = writers.stream()
+			.filter(writer -> writer instanceof AsyncWriter)
+			.map(writer -> (AsyncWriter) writer)
+			.collect(Collectors.toList());
+
+		return asyncWriters.isEmpty() ? null : new WritingThread(asyncWriters, QUEUE_SIZE);
+	}
+
+}
---
+++ tinylog-impl/src/test/java/org/tinylog/impl/backend/NativeLoggingBackendBuilderTest.java
@@ -0,0 +1,103 @@
+package org.tinylog.impl.backend;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ServiceLoader;
+
+import org.junit.jupiter.api.Test;
+import org.tinylog.core.Framework;
+import org.tinylog.core.Level;
+import org.tinylog.core.backend.LoggingBackend;
+import org.tinylog.core.backend.LoggingBackendBuilder;
+import org.tinylog.core.runtime.StackTraceLocation;
+import org.tinylog.impl.writers.ConsoleWriter;
+import org.tinylog.impl.writers.FileWriter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class NativeLoggingBackendBuilderTest {
+
+	/**
+	 * Verifies that a {@link NativeLoggingBackend} with a sync {@link ConsoleWriter} can be created and used for
+	 * logging.
+	 */
+	@Test
+	void syncConsoleWriter() {
+		PrintStream mockedOutputStream = mock(PrintStream.class);
+		PrintStream originalOutputStream = System.out;
+		System.setOut(mockedOutputStream);
+
+		try {
+			Framework framework = new Framework(false, false);
+			try {
+				framework.getConfiguration().set("level", "INFO");
+				framework.getConfiguration().set("writer.type", "console");
+				framework.getConfiguration().set("writer.pattern", "{level}:{message}");
+				framework.startUp();
+
+				LoggingBackend backend = framework.getLoggingBackend();
+				assertThat(backend).isInstanceOf(NativeLoggingBackend.class);
+
+				StackTraceLocation location = framework.getRuntime().getStackTraceLocationAtIndex(0);
+				backend.log(location.push(), null, Level.DEBUG, null, "Hello World!", null, null);
+				backend.log(location.push(), null, Level.INFO, null, "Hello World!", null, null);
+			} finally {
+				framework.shutDown();
+			}
+
+			verify(mockedOutputStream).print("INFO:Hello World!" + System.lineSeparator());
+		} finally {
+			System.setOut(originalOutputStream);
+		}
+	}
+
+	/**
+	 * Verifies that a {@link NativeLoggingBackend} with an async {@link FileWriter} can be created and used for
+	 * logging.
+	 */
+	@Test
+	void asyncFileWriter() throws IOException {
+		Path logFile = Files.createTempFile("tinylog", ".log");
+		logFile.toFile().deleteOnExit();
+
+		try {
+			Framework framework = new Framework(false, false);
+			try {
+				framework.getConfiguration().set("level", "INFO");
+				framework.getConfiguration().set("writer.type", "file");
+				framework.getConfiguration().set("writer.file", logFile.toAbsolutePath().toString());
+				framework.getConfiguration().set("writer.pattern", "{level}:{message}");
+				framework.startUp();
+
+				LoggingBackend backend = framework.getLoggingBackend();
+				assertThat(backend).isInstanceOf(NativeLoggingBackend.class);
+
+				StackTraceLocation location = framework.getRuntime().getStackTraceLocationAtIndex(0);
+				backend.log(location.push(), null, Level.DEBUG, null, "Hello World!", null, null);
+				backend.log(location.push(), null, Level.INFO, null, "Hello World!", null, null);
+			} finally {
+				framework.shutDown();
+			}
+
+			assertThat(logFile).hasContent("INFO:Hello World!" + System.lineSeparator());
+		} finally {
+			Files.deleteIfExists(logFile);
+		}
+	}
+
+	/**
+	 * Verifies that the builder is registered as service.
+	 */
+	@Test
+	void service() {
+		assertThat(ServiceLoader.load(LoggingBackendBuilder.class)).anySatisfy(builder -> {
+			assertThat(builder).isInstanceOf(NativeLoggingBackendBuilder.class);
+			assertThat(builder.getName()).isEqualTo("tinylog");
+		});
+	}
+
+}
