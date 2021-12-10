698fd2fe2351ab8de1409d8114c76caf68f675c9
Martin Winandy <martin.winandy@pmwmedia.de>
2021-12-10 18:22:20 +0100

    Export the TinylogSlf4jServiceProvider via module-info.java
    Fix #223

--- slf4j-tinylog/src/main/java9/module-info.java
+++ slf4j-tinylog/src/main/java9/module-info.java
@@ -2,6 +2,9 @@ module org.tinylog.api.slf4j {
 	requires org.tinylog.api;
 	requires org.slf4j;
 
+	provides org.slf4j.spi.SLF4JServiceProvider with
+		org.tinylog.slf4j.TinylogSlf4jServiceProvider;
+
 	exports org.slf4j.impl;
 	exports org.tinylog.slf4j;
 }
