31e3ce75a4adce6a9788b62c68d421aa475aa96b
Martin Winandy <martin.winandy@pmwmedia.de>
2022-01-17 23:40:54 +0100

    Add threat context object for Kotlin logging API

---
+++ tinylog-api-kotlin/src/main/kotlin/org/tinylog/kotlin/ThreadContext.kt
@@ -0,0 +1,99 @@
+package org.tinylog.kotlin
+
+import org.tinylog.core.Tinylog
+
+/**
+ * Thread context stores additional thread-based context values for issued log entries. A stored context value is added
+ * to all log entries issued by the same thread where the context value was set. In web applications for example, the IP
+ * address or the username are typical context values to store. The output of context values can be configured via
+ * format patterns and writers.
+ *
+ * Depending on the actual logging back-end, child threads can inherit the thread-based context value of its
+ * parent thread.
+ *
+ * Stored context values should be cleared before returning a thread to a thread pool. This can be either done by
+ * calling [clear] after the actual code execution or by wrapping the actual code itself with the methods
+ * [withIndependentContext] or [withEmptyContext].
+ */
+object ThreadContext {
+	private val storage = Tinylog.getFramework().loggingBackend.contextStorage
+
+	/**
+	 * Retrieves a copy of all stored context values.
+	 *
+	 * The returned map is not intended to be modified. Modification methods of the returned map can throw runtime
+	 * exceptions.
+	 */
+	val mapping: Map<String, String>
+		get() = storage.mapping
+
+	/**
+	 * Retrieves the stored context value for a given key.
+	 *
+	 * @param key The key of the requested context value
+	 * @return The stored value for the passed key, or `null` if no value is stored for the passed key
+	 */
+	operator fun get(key: String): String? {
+		return storage[key]
+	}
+
+	/**
+	 * Stores a new context value for a given key. If a value is already stored for the passed key, the existent value
+	 * is overwritten.
+	 *
+	 * If the passed value is neither `null` nor a string, it will be converted to a string by calling its
+	 * `toString()` method.
+	 *
+	 * @param key The key with which the context value is to be associated
+	 * @param value The context value to store
+	 */
+	fun put(key: String, value: Any?) {
+		storage.put(key, value?.toString())
+	}
+
+	/**
+	 * Removes the stored context value for a given key.
+	 *
+	 * If no value is associated with the passed key, this is a no-op. No exception will be thrown.
+	 *
+	 * @param key The key whose associated context value is to be removed
+	 */
+	fun remove(key: String) {
+		storage.remove(key)
+	}
+
+	/**
+	 * Removes all stored context values. Afterwards, the thread context will be empty.
+	 */
+	fun clear() {
+		storage.clear()
+	}
+
+	/**
+	 * Executes the passed command with the actual thread context. After the command execution, all thread context
+	 * modification will be undone and the origin thread context will be restored.
+	 *
+	 * @param command The code to execute
+	 */
+	fun withIndependentContext(command: () -> Unit) {
+		val mapping = storage.mapping
+		try {
+			command()
+		} finally {
+			storage.replace(mapping)
+		}
+	}
+
+	/**
+	 * Executes the passed command with an empty thread context. After the command execution, all thread context
+	 * modification will be undone and the origin thread context will be restored.
+	 *
+	 * @param command The code to execute
+	 */
+	fun withEmptyContext(command: () -> Unit) {
+		withIndependentContext {
+			storage.clear()
+			command()
+		}
+	}
+}
---
+++ tinylog-api-kotlin/src/test/kotlin/org/tinylog/kotlin/ThreadContextTest.kt
@@ -0,0 +1,160 @@
+package org.tinylog.kotlin
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.mockito.ArgumentMatchers.anyMap
+import org.mockito.MockedStatic
+import org.mockito.Mockito.mockStatic
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.never
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.tinylog.core.Framework
+import org.tinylog.core.Tinylog
+import org.tinylog.core.backend.LoggingBackend
+import org.tinylog.core.context.ContextStorage
+
+internal class ThreadContextTest {
+	companion object {
+		private lateinit var storage: ContextStorage
+		private lateinit var backend: LoggingBackend
+		private lateinit var tinylogMock: MockedStatic<Tinylog>
+
+		/**
+		 * Initializes all mocks.
+		 */
+		@BeforeAll
+		@JvmStatic
+		fun create() {
+			storage = mock()
+
+			backend = mock() {
+				on { contextStorage } doReturn storage
+			}
+
+			tinylogMock = mockStatic(Tinylog::class.java).apply {
+				`when`<Any> {
+					Tinylog.getFramework()
+				}.thenReturn(object : Framework(false, false) {
+					override fun getLoggingBackend() = backend
+				})
+			}
+		}
+
+		/**
+		 * Restores the mocked tinylog class.
+		 */
+		@AfterAll
+		@JvmStatic
+		fun dispose() {
+			tinylogMock.close()
+		}
+	}
+
+	/**
+	 * Resets the context storage.
+	 */
+	@AfterEach
+	fun reset() {
+		org.mockito.kotlin.reset(storage)
+	}
+
+	/**
+	 * Verifies that all stored kwy value pairs can be received.
+	 */
+	@Test
+	fun receiveMapping() {
+		whenever(storage.mapping).thenReturn(mapOf("foo" to "42"))
+		assertThat(ThreadContext.mapping).isEqualTo(mapOf("foo" to "42"))
+	}
+
+	/**
+	 * Verifies that a value can be received by its associated key.
+	 */
+	@Test
+	fun receiveValue() {
+		whenever(storage["foo"]).thenReturn("42")
+		assertThat(ThreadContext["foo"]).isEqualTo("42")
+	}
+
+	/**
+	 * Verifies that `null` can be stored as value.
+	 */
+	@Test
+	fun putNull() {
+		ThreadContext.put("foo", null)
+		verify(storage).put("foo", null)
+	}
+
+	/**
+	 * Verifies that an integer will be stored as string.
+	 */
+	@Test
+	fun putInteger() {
+		ThreadContext.put("foo", 42)
+		verify(storage).put("foo", "42")
+	}
+
+	/**
+	 * Verifies that a string value can be stored.
+	 */
+	@Test
+	fun putString() {
+		ThreadContext.put("foo", "bar")
+		verify(storage).put("foo", "bar")
+	}
+
+	/**
+	 * Verifies that a value can be removed by its associated key.
+	 */
+	@Test
+	fun removeValue() {
+		ThreadContext.remove("foo")
+		verify(storage).remove("foo")
+	}
+
+	/**
+	 * Verifies that all stored values can be removed.
+	 */
+	@Test
+	fun clearAllValues() {
+		ThreadContext.clear()
+		verify(storage).clear()
+	}
+
+	/**
+	 * Verifies that the original mapping will be restored after executing code with an independent context.
+	 */
+	@Test
+	fun executeIndependentContext() {
+		whenever(storage.mapping).thenReturn(mapOf("foo" to "42"))
+
+		ThreadContext.withIndependentContext {
+			whenever(storage.mapping).thenThrow(UnsupportedOperationException::class.java)
+			verify(storage, never()).clear()
+			verify(storage, never()).replace(anyMap())
+		}
+
+		verify(storage).replace(mapOf("foo" to "42"))
+	}
+
+	/**
+	 * Verifies that the original mapping will be restored after executing code with an empty context.
+	 */
+	@Test
+	fun executionEmptyContext() {
+		whenever(storage.mapping).thenReturn(mapOf("foo" to "42"))
+
+		ThreadContext.withEmptyContext {
+			whenever(storage.mapping).thenThrow(UnsupportedOperationException::class.java)
+			verify(storage).clear()
+			verify(storage, never()).replace(anyMap())
+		}
+
+		verify(storage).replace(mapOf("foo" to "42"))
+	}
+}
