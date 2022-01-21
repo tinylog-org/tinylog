5bb1d034951547979e757d56e79c939c5b55dc16
Martin Winandy <martin.winandy@pmwmedia.de>
2022-01-21 23:06:38 +0100

    Add no-operation benchmark as reference value for logging benchmarks

---
+++ benchmarks/benchmark-logging/benchmark-noop/src/main/java/org/tinylog/benchmarks/logging/noop/NoOperationBenchmark.java
@@ -0,0 +1,24 @@
+package org.tinylog.benchmarks.logging.noop;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+
+/**
+ * No-operation benchmark as reference value for logging benchmarks.
+ */
+public class NoOperationBenchmark {
+
+	/** */
+	public NoOperationBenchmark() {
+	}
+
+	/**
+	 * Does nothing as a no-operation benchmark method.
+	 */
+	@Benchmark
+	@BenchmarkMode(Mode.Throughput)
+	public void emptyMethod() {
+	}
+
+}
---
+++ benchmarks/benchmark-logging/benchmark-noop/src/main/java/org/tinylog/benchmarks/logging/noop/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JMH benchmarks for no-operation methods as reference value for logging benchmarks.
+ */
+package org.tinylog.benchmarks.logging.noop;
