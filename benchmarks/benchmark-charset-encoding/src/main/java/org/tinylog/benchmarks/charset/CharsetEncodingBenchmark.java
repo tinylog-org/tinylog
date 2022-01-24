2337188b69295b1a01e7e2a04448f59dcd7a1be0
Martin Winandy <martin.winandy@pmwmedia.de>
2022-01-24 20:35:14 +0100

    Integrate state into charset encoding benchmark

--- benchmarks/benchmark-charset-encoding/src/main/java/org/tinylog/benchmarks/charset/CharsetEncodingBenchmark.java
+++ benchmarks/benchmark-charset-encoding/src/main/java/org/tinylog/benchmarks/charset/CharsetEncodingBenchmark.java
@@ -8,40 +8,69 @@ import java.nio.charset.CoderResult;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 
 /**
  * Benchmark for comparing the performance of different approaches for encoding strings.
  */
+@State(Scope.Thread)
 public class CharsetEncodingBenchmark {
+
+	/**
+	 * The name of the charset.
+	 */
+	@Param({ "US-ASCII", "ISO-8859-1", "UTF-8", "UTF-16" })
+	public String charsetName;
+
+	private static final int MAX_BYTES_PER_CHAR = 4;
+	private static final String CONTENT = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy"
+		+ " eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam"
+		+ " et justo duo dolores.";
+
+	public Charset charset;
+	public CharsetEncoder encoder;
+	public ByteBuffer buffer;
 
 	/** */
 	public CharsetEncodingBenchmark() {
 	}
 
 	/**
+	 * Initializes the member fields.
+	 */
+	@Setup(Level.Trial)
+	public void init() {
+		charset = Charset.forName(charsetName);
+		encoder = charset.newEncoder();
+		buffer = ByteBuffer.allocate(CONTENT.length() * MAX_BYTES_PER_CHAR);
+	}
+
+	/**
 	 * Uses {@link String#getBytes(Charset)} for encoding.
 	 *
-	 * @param state Arguments and data for the benchmark
 	 * @return The byte array for the encoded string
 	 */
 	@Benchmark
 	@BenchmarkMode(Mode.Throughput)
-	public byte[] getBytes(CharsetState state) {
-		return state.content.getBytes(state.charset);
+	public byte[] getBytes() {
+		return CONTENT.getBytes(charset);
 	}
 
 	/**
 	 * Uses {@link CharsetEncoder#encode(CharBuffer, ByteBuffer, boolean)} for encoding.
 	 *
-	 * @param state Arguments and data for the benchmark
 	 * @return The coder result for the encoded string
 	 */
 	@Benchmark
 	@BenchmarkMode(Mode.Throughput)
-	public CoderResult encode(CharsetState state) {
-		CoderResult result = state.encoder.encode(CharBuffer.wrap(state.content), state.buffer, true);
-		state.buffer.rewind();
+	public CoderResult encode() {
+		CoderResult result = encoder.encode(CharBuffer.wrap(CONTENT), buffer, true);
+		buffer.rewind();
 		return result;
 	}
 
