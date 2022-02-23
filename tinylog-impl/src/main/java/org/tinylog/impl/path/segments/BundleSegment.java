2ee420bb97d69e888ab8f3633ae996eb0e8e7eb2
Martin Winandy <martin.winandy@pmwmedia.de>
2022-02-23 21:29:41 +0100

    Add BundleSegment for combining multiple path segments

---
+++ tinylog-impl/src/main/java/org/tinylog/impl/path/segments/BundleSegment.java
@@ -0,0 +1,32 @@
+package org.tinylog.impl.path.segments;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+/**
+ * Bundle of multiple child path segments.
+ *
+ * <p>
+ *     This bundle segment combines the resolving of multiple child path segments. All child path segments are
+ *     resolved and combined in the order in which they have been passed.
+ * </p>
+ */
+public class BundleSegment implements PathSegment {
+
+	private final List<PathSegment> segments;
+
+	/**
+	 * @param segments Child path segments
+	 */
+	public BundleSegment(List<PathSegment> segments) {
+		this.segments = segments;
+	}
+
+	@Override
+	public void resolve(StringBuilder pathBuilder, ZonedDateTime date) {
+		for (PathSegment segment : segments) {
+			segment.resolve(pathBuilder, date);
+		}
+	}
+
+}
