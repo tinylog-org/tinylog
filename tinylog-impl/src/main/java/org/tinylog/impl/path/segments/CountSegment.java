3e49fb1caf90d2a80040dd46e9cfc94c01a16577
Martin Winandy <martin.winandy@pmwmedia.de>
2022-02-24 08:05:44 +0100

    Add path segment for sequential count

---
+++ tinylog-impl/src/main/java/org/tinylog/impl/path/segments/CountSegment.java
@@ -0,0 +1,79 @@
+package org.tinylog.impl.path.segments;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.ZonedDateTime;
+
+/**
+ * Path segment for a sequential count.
+ */
+public class CountSegment implements PathSegment {
+
+	/** */
+	public CountSegment() {
+	}
+
+	@Override
+	public void resolve(StringBuilder pathBuilder, ZonedDateTime date) throws IOException {
+		Path expandedPath = Paths.get(pathBuilder + "_"); // Add "_" as dummy to avoid getting the parent directory
+		Path parentPath = expandedPath.getParent();
+
+		String expandedName = expandedPath.getFileName().toString();
+		String namePrefix = expandedName.substring(0, expandedName.length() - 1); // Without the dummy "_"
+
+		long currentMax;
+		try {
+			currentMax = Files.list(parentPath)
+				.map(path -> path.getFileName().toString())
+				.filter(name -> name.startsWith(namePrefix))
+				.mapToLong(name -> getCount(name, namePrefix))
+				.max()
+				.orElse(-1);
+		} catch (NoSuchFileException ex) {
+			currentMax = -1;
+		}
+
+		if (currentMax < 0 || currentMax == Long.MAX_VALUE) {
+			pathBuilder.append('0');
+		} else {
+			pathBuilder.append(currentMax + 1);
+		}
+	}
+
+	/**
+	 * Extracts the sequential count from a file name.
+	 *
+	 * @param fileName The file name
+	 * @param prefix The plain static prefix before the sequential count
+	 * @return The extracted count or {@code -1} if the file name doesn't contain a valid numeric count
+	 */
+	private static long getCount(String fileName, String prefix) {
+		int start = prefix.length();
+		int end = prefix.length();
+
+		while (end < fileName.length() && isDigit(fileName.charAt(end))) {
+			end += 1;
+		}
+
+		try {
+			String count = fileName.substring(start, end);
+			return Long.parseLong(count);
+		} catch (NumberFormatException ex) {
+			return -1;
+		}
+	}
+
+	/**
+	 * Tests if a character is an ASCII digit ('0' - '9').
+	 *
+	 * @param character The character to test
+	 * @return {@code true} if the character is in a digit, otherwise {@code false}
+	 */
+	private static boolean isDigit(char character) {
+		return character >= '0' && character <= '9';
+	}
+
+}
---
+++ tinylog-impl/src/test/java/org/tinylog/impl/path/segments/CountSegmentTest.java
@@ -0,0 +1,112 @@
+package org.tinylog.impl.path.segments;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CountSegmentTest {
+
+	@TempDir
+	private Path folder;
+
+	/**
+	 * Verifies that the count starts with "0" if no predecessor exists.
+	 */
+	@Test
+	void firstCount() throws IOException {
+		StringBuilder builder = new StringBuilder(folder.resolve("bar").toString());
+		new CountSegment().resolve(builder, null);
+		assertThat(builder).asString().isEqualTo(folder.resolve("bar0").toString());
+	}
+
+	/**
+	 * Verifies that the count resumes with "1" if a predecessor exists with "0".
+	 */
+	@Test
+	void secondCount() throws IOException {
+		Files.createFile(folder.resolve("bar0"));
+
+		StringBuilder builder = new StringBuilder(folder.resolve("bar").toString());
+		new CountSegment().resolve(builder, null);
+		assertThat(builder).asString().isEqualTo(folder.resolve("bar1").toString());
+	}
+
+	/**
+	 * Verifies that the count resumes with "2" if predecessors exist with "0" and "1".
+	 */
+	@Test
+	void thirdCount() throws IOException {
+		Files.createFile(folder.resolve("bar0"));
+		Files.createFile(folder.resolve("bar1"));
+
+		StringBuilder builder = new StringBuilder(folder.resolve("bar").toString());
+		new CountSegment().resolve(builder, null);
+		assertThat(builder).asString().isEqualTo(folder.resolve("bar2").toString());
+	}
+
+	/**
+	 * Verifies that the count resumes with "9223372036854775807" if a predecessor exists with "9223372036854775806".
+	 */
+	@Test
+	void secondToLastCount() throws IOException {
+		Files.createFile(folder.resolve("bar" + (Long.MAX_VALUE - 1)));
+
+		StringBuilder builder = new StringBuilder(folder.resolve("bar").toString());
+		new CountSegment().resolve(builder, null);
+		assertThat(builder).asString().isEqualTo(folder.resolve("bar").toString() + Long.MAX_VALUE);
+	}
+
+	/**
+	 * Verifies that the count resumes with "0" if a predecessor exists with "9223372036854775807".
+	 */
+	@Test
+	void lastCount() throws IOException {
+		Files.createFile(folder.resolve("bar" + Long.MAX_VALUE));
+
+		StringBuilder builder = new StringBuilder(folder.resolve("bar").toString());
+		new CountSegment().resolve(builder, null);
+		assertThat(builder).asString().isEqualTo(folder.resolve("bar0").toString());
+	}
+
+	/**
+	 * Verifies that the count works without any file prefix.
+	 */
+	@Test
+	void rootCount() throws IOException {
+		Files.createFile(folder.resolve("0"));
+
+		StringBuilder builder = new StringBuilder(folder + folder.getFileSystem().getSeparator());
+		new CountSegment().resolve(builder, null);
+		assertThat(builder).asString().isEqualTo(folder.resolve("1").toString());
+	}
+
+	/**
+	 * Verifies that the count works even if the directory does not exist.
+	 */
+	@Test
+	void missingDirectory() throws IOException {
+		Files.delete(folder);
+
+		StringBuilder builder = new StringBuilder(folder.resolve("bar").toString());
+		new CountSegment().resolve(builder, null);
+		assertThat(builder).asString().isEqualTo(folder.resolve("bar0").toString());
+	}
+
+	/**
+	 * Verifies that files without any valid numeric count are ignored.
+	 */
+	@Test
+	void invalidCount() throws IOException {
+		Files.createFile(folder.resolve("barFOO"));
+
+		StringBuilder builder = new StringBuilder(folder.resolve("bar").toString());
+		new CountSegment().resolve(builder, null);
+		assertThat(builder).asString().isEqualTo(folder.resolve("bar0").toString());
+	}
+
+}
