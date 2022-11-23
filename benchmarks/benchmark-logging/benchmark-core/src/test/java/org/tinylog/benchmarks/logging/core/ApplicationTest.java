8010a23ac72a457aaaa4049801ae2067d7bb934d
Martin Winandy <martin.winandy@pmwmedia.de>
2022-11-23 08:21:05 +0100

    Simplify application test

--- benchmarks/benchmark-logging/benchmark-core/src/test/java/org/tinylog/benchmarks/logging/core/ApplicationTest.java
+++ benchmarks/benchmark-logging/benchmark-core/src/test/java/org/tinylog/benchmarks/logging/core/ApplicationTest.java
@@ -1,58 +1,37 @@
 package org.tinylog.benchmarks.logging.core;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.net.URISyntaxException;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import javax.inject.Inject;
+
 import org.junit.jupiter.api.Test;
 import org.openjdk.jmh.Main;
+import org.tinylog.core.test.system.CaptureSystemOutput;
+import org.tinylog.core.test.system.Output;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@CaptureSystemOutput
 class ApplicationTest {
 
-    private PrintStream originalOutputStream;
-    private PrintStream originalErrorStream;
-
-    /**
-     * Store original streams of {@link System#out} and {@link System#err}.
-     */
-    @BeforeEach
-    void init() {
-        originalOutputStream = System.out;
-        originalErrorStream = System.err;
-    }
-
-    /**
-     * Restores the original streams for {@link System#out} and {@link System#err}.
-     */
-    @AfterEach
-    void reset() {
-        System.setOut(originalOutputStream);
-        System.setErr(originalErrorStream);
-    }
+    @Inject
+    private Output output;
 
     /**
      * Verifies that JMH will be executed correctly.
      */
     @Test
     void executeJmh() throws URISyntaxException, IOException {
-        ByteArrayOutputStream expectedOutput = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(expectedOutput));
-        System.setErr(new PrintStream(expectedOutput));
         Main.main(new String[] {"-h"});
+        Iterable<String> expectedOutput = output.consume();
 
-        ByteArrayOutputStream actualOutput = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(actualOutput));
-        System.setErr(new PrintStream(actualOutput));
         Application.main(new String[] {"-h"});
+        Iterable<String> actualOutput = output.consume();
 
-        assertThat(actualOutput.toString())
-            .contains("JMH")
-            .isEqualTo(expectedOutput.toString());
+        assertThat(actualOutput)
+            .anySatisfy(line -> assertThat(line).contains("JMH"))
+            .isEqualTo(expectedOutput);
     }
 
 }
